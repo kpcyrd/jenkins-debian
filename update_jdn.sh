f2eb3273894a3879b67de62a7ff6f400a47f0b92
Holger Levsen <holger@layer-acht.org>
2012-11-05 00:51:06 +0100

    svn hurts

--- update_jdn.sh
+++ update_jdn.sh
@@ -44,7 +44,7 @@ fi
 sudo chown root.root /etc/sudoers.d/jenkins ; sudo chmod 700 /etc/sudoers.d/jenkins
 sudo ln -sf /etc/apache2/sites-available/jenkins.debian.net /etc/apache2/sites-enabled/000-default
 sudo service apache2 reload
-cd /etc/munin/plugins ; sudo rm -f postfix_* open_inodes df_inode interrupts diskstats irqstats threads proc_pri vmstat if_err_eth0 fw_forwarded_local forks open_files 2>/dev/null
+cd /etc/munin/plugins ; sudo rm -f postfix_* open_inodes df_inode interrupts diskstats irqstats threads proc_pri vmstat if_err_eth0 fw_forwarded_local forks open_files users 2>/dev/null
 [ -L apache_accesses ] || for i in apache_accesses apache_volume ; do ln -s /usr/share/munin/plugins/$i $i ; done
 explain "Packages configured."
 

41c0e2af6b577fb2e29b027c05ccefacff24d449
Holger Levsen <holger@layer-acht.org>
2012-11-05 00:07:12 +0100

    be more verbose

--- update_jdn.sh
+++ update_jdn.sh
@@ -3,6 +3,12 @@
 # Copyright 2012 Holger Levsen <holger@layer-acht.org>
 # released under the GPLv=2
 
+explain() {
+	echo
+	echo $@
+	echo
+}
+
 # make sure needed directories exists
 for directory in  /srv/jenkins /chroots ; do
 	if [ ! -d $directory ] ; then
@@ -12,19 +18,13 @@ for directory in  /srv/jenkins /chroots ; do
 done
 
 #
-# install the heart of jenkins.debian.net
-#
-cp -r bin logparse job-cfg /srv/jenkins/
-cp -r userContent/* /var/lib/jenkins/userContent/
-asciidoc -a numbered -a data-uri -a iconsdir=/etc/asciidoc/images/icons -a scriptsdir=/etc/asciidoc/javascripts -a imagesdir=./  -b html5 -a toc -a toclevels=4 -a icons -o about.html TODO && cp about.html /var/lib/jenkins/userContent/ && echo Updated about.html
-
-#
 # install packages we need
 # (more or less grouped into more-then-nice-to-have, needed-while-things-are-new, needed)
 #
 sudo apt-get install vim screen less etckeeper curl mtr-tiny dstat devscripts bash-completion shorewall shorewall6 cron-apt apt-listchanges munin \
 	build-essential python-setuptools \
 	debootstrap sudo figlet graphviz apache2 python-yaml python-pip mr subversion subversion-tools vnstat
+explain "Packages installed."
 
 #
 # deploy package configuration in /etc
@@ -43,6 +43,15 @@ sudo ln -sf /etc/apache2/sites-available/jenkins.debian.net /etc/apache2/sites-e
 sudo service apache2 reload
 cd /etc/munin/plugins ; sudo rm -f postfix_* open_inodes df_inode interrupts diskstats irqstats threads proc_pri vmstat 2>/dev/null
 [ -L apache_accesses ] || for i in apache_accesses apache_processes apache_volume ; do ln -s /usr/share/munin/plugins/$i $i ; done
+explain "Packages configured."
+
+#
+# install the heart of jenkins.debian.net
+#
+cp -r bin logparse job-cfg /srv/jenkins/
+cp -r userContent/* /var/lib/jenkins/userContent/
+asciidoc -a numbered -a data-uri -a iconsdir=/etc/asciidoc/images/icons -a scriptsdir=/etc/asciidoc/javascripts -a imagesdir=./  -b html5 -a toc -a toclevels=4 -a icons -o about.html TODO && cp about.html /var/lib/jenkins/userContent/ && echo Updated about.html
+explain "Jenkins updated."
 
 #
 # run jenkins-job-builder to update jobs if needed
@@ -50,4 +59,5 @@ cd /etc/munin/plugins ; sudo rm -f postfix_* open_inodes df_inode interrupts dis
 #
 cd /srv/jenkins/job-cfg 
 sudo jenkins-jobs update .
+explain "Jenkins jobs updated."
 

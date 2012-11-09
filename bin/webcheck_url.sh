4012c47dd42b91af115bfb226ebb84d07441f6ca
Holger Levsen <holger@layer-acht.org>
2012-11-09 14:53:40 +0100

    add simple webcheck job

---
+++ bin/webcheck_url.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright 2012 Holger Levsen <holger@layer-acht.org>
+# released under the GPLv=2
+
+# $1 = URL
+
+if [ "$1" == "" ] ; then
+	echo "need at least one URL to act on"
+	echo '# $1 = URL'
+	exit 1
+fi
+
+#set -x
+set -e
+export LANG=C
+export http_proxy="http://localhost:3128"
+
+TMPFILE=$(mktemp -d)
+cd $TMPFILE
+PARAMS=""
+if [ "$2" != "" ] ; then
+	PARAMS=$(for i in $2 ; do echo -n "-y $i " ; done)
+fi
+webcheck $1 $PARAMS
+cd ..
+mv $TMPFILE $(basename $1)

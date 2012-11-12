d4b1609b4b5b966b0882f0fc8d9d7c18a9f2b578
Holger Levsen <holger@layer-acht.org>
2012-11-12 11:29:59 +0100

    be more verbose

--- bin/webcheck_url.sh
+++ bin/webcheck_url.sh
@@ -36,6 +36,7 @@ fi
 # if $URL ends with / then run webcheck with -b
 #
 if [ "${URL: -1}" = "/" ] ; then
+	echo "URL ending in / - adding '-b' to parameters."
 	PARAMS="$PARAMS -b"
 fi
 
@@ -43,6 +44,7 @@ fi
 # ignore some extra patterns (=all translations) when checking www.debian.org
 #
 if [ "${URL:0:21}" = "http://www.debian.org" ] ; then
+	echo "URL starts with http://www.debian.org - so better ignore lots of translated documents. (Checking translations is out of scope for this test.)"
 	# originly was TRANSLATIONS=$(curl www.debian.org 2>/dev/null|grep index|grep lang=|cut -d "." -f2)
 	# but then I had to add some and then some more... so I reached to the conclusion to hardcode them all
 	TRANSLATIONS="ar bg ca cs da de el es eo fa fr ko hy hr id it he lt hu nl ja nb pl pt ro ru sk fi sv ta tr uk zh-cn zh-hk zh-tw ml vi"
@@ -55,6 +57,7 @@ fi
 # ignore some extra patterns (=the installation manual for all releases and all archs) when checking www.debian.org
 #
 if [ "${URL:0:21}" = "http://www.debian.org" ] && [ "${URL: -1}" != "/" ] ; then
+	echo "URL is http://www.debian.org - so better ignore all those manuals for all releases (and the architecture permutations). (Checking these manuals is out of scope for this test.)"
 	RELEASES="slink potato woody sarge etch lenny squeeze wheezy stable"
 	SLINK="i386 m68k alpha sparc"
 	POTATO="$SLINK powerpc arm"
@@ -80,7 +83,6 @@ if [ "${URL:0:21}" = "http://www.debian.org" ] && [ "${URL: -1}" != "/" ] ; then
 	fi
 fi
 
-
 #
 # $PATTERNS can only be used to ignore patterns atm
 #
@@ -91,4 +93,6 @@ fi
 #
 # actually run webcheck
 #
+echo "Now running: webcheck $URL $PARAMS"
+echo
 webcheck $URL $PARAMS

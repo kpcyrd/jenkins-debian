ca11e09f3474335891649fa7a0d132f1610aec6f
Holger Levsen <holger@layer-acht.org>
2012-11-12 09:47:23 +0100

    fixup: add job to check for missing d-i jobs

--- bin/d-i_check_jobs.sh
+++ bin/d-i_check_jobs.sh
@@ -11,9 +11,13 @@ set -e
 export LC_ALL=C
 export http_proxy="http://localhost:3128"
 
+#
+# define some variables
+#
 URL="http://anonscm.debian.org/viewvc/d-i/trunk/.mrconfig?view=co"
 FAIL=false
 DI_JOBPATTERN=d-i_build_
+TMPFILE=$(mktemp)
 
 #
 # check for missing d-i package build jobs
@@ -21,10 +25,10 @@ DI_JOBPATTERN=d-i_build_
 # 	(see http://wiki.debian.org/DebianInstaller/CheckOut)
 #
 echo "Scanning $URL for reffered git repos which have no jenkins job associated."
-curl $URL 2>/dev/null
+curl $URL > $TMPFILE 2>/dev/null
 for PACKAGE in $( grep git.debian.org/git/d-i $TMPFILE|cut -d "/" -f6-) ; do
 	#
-	# check if a jenkins job exist
+	# check if a jenkins job exists
 	#
 	if [ ! -d ~jenkins/jobs/${DI_JOBPATTERN}${PACKAGE} ] ; then
 		echo "Warning: No build job \'${DI_JOBPATTERN}${PACKAGE}\'."
@@ -34,6 +38,7 @@ for PACKAGE in $( grep git.debian.org/git/d-i $TMPFILE|cut -d "/" -f6-) ; do
 	fi
 done
 echo
+rm $TMPFILE
 
 #
 # check for missing d-i manual lanague build jobs

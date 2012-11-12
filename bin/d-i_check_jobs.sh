2b84e162d4d8d516ca1389c81e2fc8c24ec2f780
Holger Levsen <holger@layer-acht.org>
2012-11-12 09:44:28 +0100

    add job to check for missing d-i jobs

---
+++ bin/d-i_check_jobs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Copyright 2012 Holger Levsen <holger@layer-acht.org>
+# released under the GPLv=2
+
+#
+# default settings
+#
+set -x
+set -e
+export LC_ALL=C
+export http_proxy="http://localhost:3128"
+
+URL="http://anonscm.debian.org/viewvc/d-i/trunk/.mrconfig?view=co"
+FAIL=false
+DI_JOBPATTERN=d-i_build_
+
+#
+# check for missing d-i package build jobs
+# for this, we compare referred git repos in .mrconfig against locally existing jenkins jobs
+# 	(see http://wiki.debian.org/DebianInstaller/CheckOut)
+#
+echo "Scanning $URL for reffered git repos which have no jenkins job associated."
+curl $URL 2>/dev/null
+for PACKAGE in $( grep git.debian.org/git/d-i $TMPFILE|cut -d "/" -f6-) ; do
+	#
+	# check if a jenkins job exist
+	#
+	if [ ! -d ~jenkins/jobs/${DI_JOBPATTERN}${PACKAGE} ] ; then
+		echo "Warning: No build job \'${DI_JOBPATTERN}${PACKAGE}\'."
+		FAIL=true
+	else
+		echo "Ok: Job \'${DI_JOBPATTERN}${PACKAGE}\' exists."
+	fi
+done
+echo
+
+#
+# check for missing d-i manual lanague build jobs
+#
+# FIXME: implement this check ;-)
+
+#
+# fail this job if missing d-i jobs are detected
+#
+if $FAIL ; then 
+	figlet "Missing jobs!"
+	exit 1
+else
+	figlet ok
+fi

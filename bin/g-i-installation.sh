49f2964dfbbf72defcdc14a1a3bb7ba05f075d23
Holger Levsen <holger@layer-acht.org>
2013-02-05 21:28:43 +0100

    fixup

--- bin/g-i-installation.sh
+++ bin/g-i-installation.sh
@@ -495,6 +495,7 @@ normal_action() {
 							;;
 						*)	;;
 					esac
+					;;
 		debian-edu*)	case $TOKEN in
 					# debian-edu installations report error found during installation, go forward
 					040)	do_and_report key enter

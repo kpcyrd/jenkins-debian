45859f3e1444491ce368265506e6c4292ef01f99
Holger Levsen <holger@layer-acht.org>
2013-04-29 23:42:33 +0200

    remove workaround for #705452

--- bin/chroot-installation.sh
+++ bin/chroot-installation.sh
@@ -89,8 +89,6 @@ prepare_upgrade2() {
 echo "deb $MIRROR $1 main contrib non-free" > /etc/apt/sources.list
 $SCRIPT_HEADER
 apt-get update
-# workaround #705452
-(dpkg -l xml-core && apt-get -y install xml-core) || true
 apt-get -y upgrade
 apt-get -y dist-upgrade
 apt-get -y dist-upgrade

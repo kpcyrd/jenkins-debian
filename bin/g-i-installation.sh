23f2fb83fc46ccf46ee9d686628ba7c4b7d85339
Holger Levsen <holger@layer-acht.org>
2013-05-02 12:44:22 +0200

    raise limit to 16h

--- bin/g-i-installation.sh
+++ bin/g-i-installation.sh
@@ -728,7 +728,7 @@ monitor_system() {
 	TRIGGER_NR=0
 	cd $RESULTS
 	sleep 4
-	hourlimit=12 # hours
+	hourlimit=16 # hours
 	echo "Taking screenshots every 2 seconds now, until qemu ends for whatever reasons or $hourlimit hours have passed or if the test seems to hang."
 	echo
 	timelimit=$(( $hourlimit * 60 * 60 / 2 ))

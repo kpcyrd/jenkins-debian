ef21849e646c1a0712735583409c4490935f8145
Joachim Breitner <mail@joachim-breitner.de>
2014-04-16 17:00:14 +0200

    First shot at a schroot creating script
    It currently uses sudo freely -- I wonder if it is worth updating
    sudoers appropriately. Maybe the whole script can be added to sudoers?
    
    The jenkins job definition itself is not tested.

---
+++ bin/schroot-install.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# Copyright 2012-2014 Holger Levsen <holger@layer-acht.org>
+# Copyright      2013 Antonio Terceiro <terceiro@debian.org>
+# released under the GPLv=2
+
+# $2 = schroot name
+# $1 = base distro
+# $2 $3 ... = extra packages to install
+
+# bootstraps a new chroot for schroot, and then moves it into the right location
+
+set -e
+export LC_ALL=C
+
+# Defaults for the jenkins.debian.net environment
+if [ -z "$MIRROR" ]; then
+	export MIRROR=http://ftp.de.debian.org/debian
+fi
+if [ -z "$http_proxy" ]; then
+	# export http_proxy="http://localhost:3128"
+	:
+fi
+if [ -z "$CHROOT_BASE" ]; then
+	export CHROOT_BASE=/chroots
+fi
+if [ -z "$SCHROOT_BASE" ]; then
+	export SCHROOT_BASE=$CHROOT_BASE/schroot
+fi
+
+if [ $# -lt 2 ]; then
+	echo "usage: $0 DISTRO [backports] CMD [ARG1 ARG2 ...]"
+	exit 1
+fi
+
+TARGET="$1"
+shift
+DISTRO="$1"
+shift
+
+if [ "$1" == "backports" ] ; then
+	BACKPORTS="deb $MIRROR ${DISTRO}-backports main"
+	BACKPORTSSRC="deb-src $MIRROR ${DISTRO}-backports main"
+	shift
+fi
+
+if [ ! -d "$CHROOT_BASE" ]; then
+	echo "Directory $CHROOT_BASE does not exist, aborting."
+	exit 1
+fi
+
+export CHROOT_TARGET=$(mktemp -d -p $CHROOT_BASE/ schroot-install-$TARGET-XXXX)
+if [ -z "$CHROOT_TARGET" ]; then
+	echo "Could not create a directory to create the chroot in, aborting."
+	exit 1
+fi
+
+export CURDIR=$(pwd)
+
+bootstrap() {
+	sudo debootstrap $DISTRO $CHROOT_TARGET $MIRROR
+
+	sudo -s <<-EOF
+		set -e
+		set -x
+		echo -e '#!/bin/sh\nexit 101' > $CHROOT_TARGET/usr/sbin/policy-rc.d
+		chmod +x $CHROOT_TARGET/usr/sbin/policy-rc.d
+		echo 'Acquire::http::Proxy "$http_proxy";' > $CHROOT_TARGET/etc/apt/apt.conf.d/80proxy
+		echo "deb-src $MIRROR $DISTRO main" >> $CHROOT_TARGET/etc/apt/sources.list
+		echo "${BACKPORTS}" >> $CHROOT_TARGET/etc/apt/sources.list
+		echo "${BACKPORTSSRC}" >> $CHROOT_TARGET/etc/apt/sources.list
+		EOF
+}
+
+cleanup() {
+	if [ -d $CHROOT_TARGET ]; then
+		sudo rm -rf --one-file-system $CHROOT_TARGET || fuser -mv $CHROOT_TARGET
+	fi
+}
+trap cleanup INT TERM EXIT
+bootstrap
+
+trap - INT TERM EXIT
+
+# pivot the new schroot in place
+rand=$RANDOM
+if [ -d $SCHROOT_BASE/"$TARGET" ]
+then
+	sudo mv $SCHROOT_BASE/"$TARGET" $SCHROOT_BASE/"$TARGET"-"$rand"
+fi
+
+sudo mv $CHROOT_TARGET $SCHROOT_BASE/"$TARGET"
+
+if [ -d $SCHROOT_BASE/"$TARGET"-"$rand" ]
+then
+	sudo rm -rf --one-file-system $SCHROOT_BASE/"$TARGET"-"$rand"
+fi
+
+# write the schroot config
+echo "Writing configuration"
+sudo tee /etc/schroot/chroot.d/jenkins-"$TARGET" <<-__END__
+	[jenkins-$TARGET]
+	description=Jenkins schroot $TARGET
+	directory=$SCHROOT_BASE/$TARGET
+	type=directory
+	root-users=jenkins
+	source-root-users=jenkins
+	union-type=aufs
+	__END__
+
+schroot -c "source:jenkins-$TARGET" -u root -- apt-get update
+if [ -n "$1" ]
+then
+	schroot -c "source:jenkins-$TARGET" -u root -- apt-get install -y --no-install-recommends "$@"
+fi
--- update_jdn.sh
+++ update_jdn.sh
@@ -12,7 +12,7 @@ explain() {
 }
 
 # make sure needed directories exists
-for directory in  /srv/jenkins /chroots ; do
+for directory in  /srv/jenkins /chroots /chroots/schroots; do
 	if [ ! -d $directory ] ; then
 		sudo mkdir $directory
 		sudo chown jenkins.jenkins $directory
@@ -25,7 +25,7 @@ done
 #
 sudo apt-get install vim screen less etckeeper moreutils curl mtr-tiny dstat devscripts bash-completion shorewall shorewall6 cron-apt apt-listchanges munin calamaris visitors procmail libjson-rpc-perl libfile-touch-perl zutils ip2host \
 	build-essential python-setuptools \
-	debootstrap sudo figlet graphviz apache2 python-yaml python-pip mr subversion subversion-tools vnstat webcheck poxml qemu vncsnapshot imagemagick ffmpeg2theora python-twisted python-imaging gocr guestmount
+	debootstrap sudo figlet graphviz apache2 python-yaml python-pip mr subversion subversion-tools vnstat webcheck poxml qemu vncsnapshot imagemagick ffmpeg2theora python-twisted python-imaging gocr guestmount schroot
 explain "Packages installed."
 
 #

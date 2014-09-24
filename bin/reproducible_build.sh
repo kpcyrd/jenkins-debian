ce33f4456e262adcfb9e47bbe497633a272cbc85
Holger Levsen <holger@layer-acht.org>
2014-09-24 23:12:04 +0200

    reproducible: nocheck prevents tests from being run

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -41,13 +41,13 @@ for SRCPACKAGE in "$@" ; do
 		SOURCELESS="${SOURCELESS} ${SRCPACKAGE}"
 		echo "Warning: ${SRCPACKAGE} is not a source package, or was removed or renamed. Please investigate."
 	else
-		sudo pbuilder --build --basetgz /var/cache/pbuilder/base-reproducible.tgz --distribution sid ${SRCPACKAGE}_*.dsc
+		sudo DEB_BUILD_OPTIONS="parallel=4 nocheck" pbuilder --build --basetgz /var/cache/pbuilder/base-reproducible.tgz --distribution sid ${SRCPACKAGE}_*.dsc
 		RESULT=$?
 		if [ $RESULT = 0 ] ; then
 			mkdir b1 b2
 			dcmd cp /var/cache/pbuilder/result/${SRCPACKAGE}_*.changes b1
 			sudo dcmd rm /var/cache/pbuilder/result/${SRCPACKAGE}_*.changes
-			sudo pbuilder --build --basetgz /var/cache/pbuilder/base-reproducible.tgz --distribution sid ${SRCPACKAGE}_*.dsc
+			sudo DEB_BUILD_OPTIONS="parallel=4 nocheck" pbuilder --build --basetgz /var/cache/pbuilder/base-reproducible.tgz --distribution sid ${SRCPACKAGE}_*.dsc
 			dcmd cp /var/cache/pbuilder/result/${SRCPACKAGE}_*.changes b2
 			sudo dcmd rm /var/cache/pbuilder/result/${SRCPACKAGE}_*.changes
 			set -e

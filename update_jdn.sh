dd4106093245c5d98d649b68d963f2c0db8ba0ef
Holger Levsen <holger@layer-acht.org>
2014-09-29 13:08:46 +0200

    fix search+replace error

--- update_jdn.sh
+++ update_jdn.sh
@@ -59,7 +59,7 @@ fi
 #
 sudo apt-get install vim screen less etckeeper moreutils curl mtr-tiny dstat devscripts bash-completion shorewall shorewall6 cron-apt apt-listchanges munin munin-plugins-extra calamaris visitors procmail libjson-rpc-perl libfile-touch-perl zutils ip2host \
 	build-essential python-setuptools \
-	debootstrap sudo figlet -f banner graphviz apache2 python-yaml python-pip mr subversion subversion-tools vnstat webcheck poxml vncsnapshot imagemagick ffmpeg2theora python-twisted python-imaging gocr guestmount schroot sqlite3\
+	debootstrap sudo figlet graphviz apache2 python-yaml python-pip mr subversion subversion-tools vnstat webcheck poxml vncsnapshot imagemagick ffmpeg2theora python-twisted python-imaging gocr guestmount schroot sqlite3\
 	unzip python-hachoir-metadata ghc
 sudo apt-get install -t wheezy-backports qemu
 explain "Packages installed."

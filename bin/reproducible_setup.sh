c21af84d77096d1ac29a219a5792087d964a92d8
Holger Levsen <holger@layer-acht.org>
2014-10-09 12:19:14 +0200

    reproducible: insert initially blacklisted packages into db

--- bin/reproducible_setup.sh
+++ bin/reproducible_setup.sh
@@ -33,6 +33,14 @@ cat >/var/lib/jenkins/reproducible.init <<-EOF
 .timeout 30000
 EOF
 
+# blacklist some packages
+for PKG in linux cups zurl openclipart ; do
+	RESULT=$(sqlite3 -init $INIT $PACKAGES_DB " SELECT name FROM source_packages WHERE status = 'blacklisted' AND name = '$PGK'")
+	if [ "$RESULT" = "" ] ; then
+		sqlite3 -init $INIT $PACKAGES_DB "REPLACE into source_packages VALUES ('$PKG','0','blacklisted',date('now'))"
+	fi
+done
+
 # FIXME: needed as long as there is no backport for debbindiff
 # 	 or as long as we dont run it within pbuilder...
 # fetch git repo for the debbindiff command used in reproducible_build.sh

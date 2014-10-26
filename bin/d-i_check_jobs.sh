57c9ad112167dd0f48bc3b312ead1b5eeeb63edb
Holger Levsen <holger@layer-acht.org>
2014-10-26 12:10:46 +0100

    remind to also delete these jobs in d-i.yaml and not only in jenkins, cause else they will be recreated

--- bin/d-i_check_jobs.sh
+++ bin/d-i_check_jobs.sh
@@ -162,6 +162,9 @@ elif [ -s $CLEANUP ] ; then
 	echo
 	cat $CLEANUP
 	echo
+	figlet "Achtung:"
+	echo "Do not forget to also delete these jobs from job-cfg/d-i.yaml - else they will be recreated and build attempted, which will fail and cause notifications..."
+	# FIXME: adopt this text once job-cfg/d-i.yaml.py generates job-cfg/d-i.yaml
 else
 	figlet ok
 fi

146031ed44e62a2f01eda3566e167f3f8726b8b2
Holger Levsen <holger@layer-acht.org>
2014-10-31 18:36:41 +0100

    email2irc: correctly truncate message for irc

--- bin/email2irc.sh
+++ bin/email2irc.sh
@@ -49,8 +49,8 @@ while read line ; do
 		if [ "${line:0:5}" = "-----" ] ; then
 			read line
 			read line
-			MY_LINE=$(read line)
-			MY_LINE=$(echo $MY_LINE | cut -d " " -f1-2)
+			read line
+			MY_LINE=$(echo $line | cut -d " " -f1-2)
 		else
 			MY_LINE=$(echo $line | tr -d \< | tr -d \>)
 		fi

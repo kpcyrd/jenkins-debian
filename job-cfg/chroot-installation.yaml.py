83bddace16ff9b0040929c7d0711825927353656
Holger Levsen <holger@layer-acht.org>
2014-11-03 12:05:51 +0100

    chroot-installation: only test jessie every two days, it's basically frozen

--- job-cfg/chroot-installation.yaml.py
+++ job-cfg/chroot-installation.yaml.py
@@ -16,7 +16,7 @@ oldstable = 'squeeze'
 # ftp.de.debian.org runs mirror updates at 03:25, 09:25, 15:25 and 21:25 UTC and usually they run 10m...
 trigger_times = { 'squeeze': '30 16 25 * *',
                   'wheezy':  '30 16 4,18 * *',
-                  'jessie':  '30 10 * * *',
+                  'jessie':  '30 10 */2 * *',
                   'sid':     '30 4 * * *' }
 
 targets = """

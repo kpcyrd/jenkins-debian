caa1ff54d703ae0f42e7180e0eb310b22d5c8a17
Samuel Thibault <samuel.thibault@ens-lyon.org>
2014-11-17 18:47:22 +0100

    g-i hurd: remove hardcoded alt-f4 simulation
    now that we have the serial log

--- bin/g-i-installation.sh
+++ bin/g-i-installation.sh
@@ -1109,10 +1109,6 @@ monitor_system() {
 		if [ ! -z "$TRIGGER_MODE" ] && [ "$TRIGGER_MODE" = "$NR" ] ; then
 			let TRIGGER_NR=NR
 		fi
-		# find out why hurd hangs
-		if [ $NR -eq 5100 ] && [[ "$NAME" =~ ^debian.*_hurd.*$ ]] ; then
-			do_and_report key alt-f4
-		fi
 		let NR=NR+1
 		sleep 2
 	done

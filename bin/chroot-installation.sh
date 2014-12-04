5389b608d065167780d97f490d9379aa8fa0ed38
Holger Levsen <holger@layer-acht.org>
2014-12-04 09:57:04 +0100

    chroot-installs: also output debootstrap command used

--- bin/chroot-installation.sh
+++ bin/chroot-installation.sh
@@ -103,7 +103,9 @@ bootstrap() {
 	echo force-unsafe-io > "$CHROOT_TARGET/etc/dpkg/dpkg.cfg.d/02dpkg-unsafe-io"
 
 	echo "Bootstraping $1 into $CHROOT_TARGET now."
+	set -x
 	sudo debootstrap $1 $CHROOT_TARGET $MIRROR
+	set +x
 	prepare_bootstrap $1
 	execute_ctmpfile 
 }

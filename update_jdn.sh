963870d841911ace7ffaa4c5f11b4f02b9864661
Holger Levsen <holger@layer-acht.org>
2014-12-19 00:52:36 +0100

    update_jdn: copy features directory into /srv/jenkins too

--- update_jdn.sh
+++ update_jdn.sh
@@ -101,7 +101,7 @@ sudo service munin-node force-reload
 # install the heart of jenkins.debian.net
 #
 cd $BASEDIR
-cp -r bin logparse job-cfg /srv/jenkins/
+cp -r bin logparse job-cfg features /srv/jenkins/
 cp procmailrc /var/lib/jenkins/.procmailrc
 explain "Jenkins updated."
 cp -pr README INSTALL TODO d-i-preseed-cfgs /var/lib/jenkins/userContent/

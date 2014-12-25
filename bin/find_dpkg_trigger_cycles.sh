7087914272837c88f91474a0837b33a60813c2f6
Holger Levsen <holger@layer-acht.org>
2014-12-25 16:14:45 +0100

    add FIXME entry so we dont forget

--- bin/find_dpkg_trigger_cycles.sh
+++ bin/find_dpkg_trigger_cycles.sh
@@ -156,6 +156,8 @@ curl "http://binarycontrol.debian.net/?q=&path=%2Ftriggers%24&format=pkglist" \
 	| while read pkg url; do
 	echo "working on $pkg..." >&2
 	mkdir DEBIAN
+	# FIXME: please revert 9280f1c87 and following as soon as Jenkins run Jessie
+	# this is about the next line:
 	curl --retry 2 --location --silent "$url" \
 		| python -c 'exec("import arpy,sys,gzip,bz2,lzma,StringIO\nar=arpy.Archive(fileobj=sys.stdin)\nfor f in ar:\n\tif f.header.name == \"control.tar.gz\":\n\t\tsys.stdout.write(gzip.GzipFile(fileobj=StringIO.StringIO(f.read())).read())\n\t\tbreak\n\telif f.header.name == \"control.tar\":\n\t\tsys.stdout.write(f.read())\n\t\tbreak\n\telif f.header.name == \"control.tar.bz2\":\n\t\tsys.stdout.write(bz2.BZ2File(fileobj=StringIO.StringIO(f.read())).read())\n\t\tbreak\n\telif f.header.name == \"control.tar.xz\":\n\t\tsys.stdout.write(lzma.LZMAFile(fileobj=StringIO.StringIO(f.read())).read())\n\t\tbreak")' \
 		| tar -C "DEBIAN" --exclude=./md5sums -x

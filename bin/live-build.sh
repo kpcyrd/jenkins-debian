fdbd3c03d1d0e1a85a9a8fc58d22e98e33e40d4d
Holger Levsen <holger@layer-acht.org>
2015-01-02 00:06:29 +0100

    live: also build debian gnome+xfce jessie live images

--- bin/live-build.sh
+++ bin/live-build.sh
@@ -12,23 +12,24 @@ cleanup_all() {
 }
 
 TMPDIR=$(mktemp --tmpdir=/srv/live-build -d)
+cd $TMPDIR
 trap cleanup_all INT TERM EXIT
 
-cd $TMPDIR
-
 # $1 is used for the hostname and username
-# $2 is standalone...
+# $2 is choosing the flavor
 lb config --distribution jessie --bootappend-live "boot=live config hostname=$1 username=$1"
 case "$2" in
 	standalone)	echo education-standalone > config/package-lists/live.list.chroot
 			;;
+	gnome)		echo gnome > config/package-lists/live.list.chroot
+			;;
+	xfce)		echo xfce4 > config/package-lists/live.list.chroot
+			;;
 	*)		;;
 esac
 sudo lb build
-ls -la *.iso || true
 mkdir -p /srv/live-build/results
-cp *.iso /srv/live-build/results
-# FIXME: use proper filenames
+cp -v live-image-amd64.hybrid.iso /srv/live-build/results/$1_$2_live_amd64.iso
 
 cleanup_all
 trap - INT TERM EXIT

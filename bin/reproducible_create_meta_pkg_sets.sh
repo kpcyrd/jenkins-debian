9bc75e3b2a88e1b5dcb296e877c8ce536a7fcdd5
Holger Levsen <holger@layer-acht.org>
2015-01-08 21:06:10 +0100

    remove duplicates first

--- bin/reproducible_create_meta_pkg_sets.sh
+++ bin/reproducible_create_meta_pkg_sets.sh
@@ -32,7 +32,7 @@ chdist --arch=$ARCH apt-get $DISTNAME update
 # helper functions
 convert_into_source_packages_only() {
 	TMP2=$(mktemp)
-	for PKG in $(cat $TMPFILE | sed "s#([^)]*)##g; s#,##g" ) ; do
+	for PKG in $(cat $TMPFILE | sed "s#([^)]*)##g; s#,##g" |sort -u ) ; do
 		SRC=""
 		if [ ! -z "$PKG" ] ; then
 			SRC=$(grep-dctrl -X -n -FPackage -sSource $PKG $PACKAGES || true )

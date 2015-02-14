7ea847808c744dc588582da77a6d0d0945784008
Holger Levsen <holger@layer-acht.org>
2015-02-14 18:56:11 +0100

    reproducible: improve wording

--- bin/reproducible_html_notes.py
+++ bin/reproducible_html_notes.py
@@ -379,8 +379,8 @@ def index_no_notes(notes, bugs):
                         'WHERE status = "unreproducible" OR status = "FTBFS"')
     without_notes = [x[0] for x in all_pkgs if x[0] not in notes]
     html = '\n<p>There are ' + str(len(without_notes)) + ' unreproducible ' \
-           + 'packages without notes.<br />This are package that nobody has ' \
-           + 'has even looked at yet.</p>\n'
+           + 'packages without notes. So these are the packages with failures ' \
+           + 'that still need to be investigated: </p>\n'
     html += '<p>\n' + tab + '<code>\n'
     html = (tab*2).join(html.splitlines(True))
     for pkg in sorted(without_notes):

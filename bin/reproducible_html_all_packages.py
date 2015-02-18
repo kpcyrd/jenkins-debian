9639fdd4594cfd8e0d65058ecd8e41775f0114c7
Mattia Rizzolo <mattia@mapreri.org>
2015-02-18 01:39:37 +0100

    reproducible: html_all_packages: quick rewrite of this tiny script in python + add support for multi release

---
+++ bin/reproducible_html_all_packages.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2015 Mattia Rizzolo <mattia@mapreri.org>
+#Based on reproducible_html_all_packages.sh © 2014 Holger Levsen <holger@layer-acht.org>
+# Licensed under GPL-2
+#
+# Depends: python3
+#
+# Build all rb-pkg pages (the pages that describe the package status) using
+# code already written in reproducible_html_packages
+
+
+from reproducible_common import *
+from reproducible_html_packages import gen_all_rb_pkg_pages
+
+
+for suite in SUITES:
+    gen_all_rb_pkg_pages(suite=suite)

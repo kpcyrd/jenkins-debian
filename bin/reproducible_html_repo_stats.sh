494d848f730dfddd7b01862312f9aa12f48792b5
Holger Levsen <holger@layer-acht.org>
2015-02-23 18:00:12 +0100

    reproducible: also list version(s) in sid

--- bin/reproducible_html_repo_stats.sh
+++ bin/reproducible_html_repo_stats.sh
@@ -24,14 +24,14 @@ sudo pbuilder --execute --basetgz /var/cache/pbuilder/base-reproducible.tgz $TMP
 grep -v ^I:\  $TMPFILE > $TMPSCRIPT
 mv $TMPSCRIPT $TMPFILE
 
-write_page "<p>The source packages are different from sid in our toolchain. They are available in an apt repository on alioth which is accessable with these sources.lists entries:<pre>"
+write_page "<p>These source packages are different from sid in our toolchain. They are available in an apt repository on alioth which is accessable with these sources.lists entries:<pre>"
 write_page "deb http://reproducible.alioth.debian.org/debian/ ./"
 write_page "deb-src http://reproducible.alioth.debian.org/debian/ ./"
 write_page "</pre></p>"
-write_page "<p><table><tr><th>source package</th><th>version(s)</th></tr>"
+write_page "<p><table><tr><th>source package</th><th>version in our repo</th><th>version in sid</th><th>cruft in our repo (if any)</th></tr>"
 SOURCES=$(grep-dctrl -n -s source -FArchitecture amd64 -o -FArchitecture all $TMPFILE | sort -u)
 for PKG in $SOURCES ; do
-	write_page "<tr><td>$PKG</td><td>"
+	write_page "<tr><td>$PKG</td>"
 	VERSIONS=$(grep-dctrl -n -s version -S $PKG $TMPFILE|sort -u)
 	CRUFT=""
 	WARN=false
@@ -44,7 +44,9 @@ for PKG in $SOURCES ; do
 			BET=${VERSION}
 		fi
 	done
-	write_page "<em>$BET</em>"
+	write_page "<td>$BET</td>"
+	SID=$(apt-cache showsrc $PKG | grep "^Version: "|cut -d " " -f2|xargs echo|sed 's# #<br />#g')
+	write_page "<td>$SID</td>"
 	for VERSION in ${VERSIONS} ; do
 		if [ "${VERSION}" != "$BET" ] ; then
 			WARN=true
@@ -53,9 +55,11 @@ for PKG in $SOURCES ; do
 	done
 	if $WARN ; then
 		echo "Warning: more than one version of $PKG available in our repo, please clean up."
-		write_page "<br />cruft: $CRUFT"
+		write_page "<td>$(echo $CRUFT|sed 's# #<br />#g')</td>"
+	else
+		write_page "<td>&nbsp;</td>"
 	fi
-	write_page "</td></tr>"
+	write_page "</tr>"
 done
 write_page "</table></p>"
 rm $TMPFILE

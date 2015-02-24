7b938e35ec3c9ad7cee0afacaf001b482844abb7
Holger Levsen <holger@layer-acht.org>
2015-02-24 18:32:15 +0100

    fix typo

--- bin/reproducible_maintainance.sh
+++ bin/reproducible_maintainance.sh
@@ -115,7 +115,7 @@ if [ -s $RESULT ] ; then
 	cat $RESULT
 	echo
 	ZOMBIES=$(echo $RESULT | cut -d " " -f1 | xargs echo)
-	echo "kill -9 $ZOMBIS"
+	echo "kill -9 $ZOMBIES"
 	echo "Please cleanup manually."
 	echo
 	DIRTY=true

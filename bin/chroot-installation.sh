06d3011df1dc26329160922122130c1e44f1ed6f
Holger Levsen <holger@layer-acht.org>
2015-02-25 20:22:24 +0100

    chroot-installation: lets try to get error-correction to work...

--- bin/chroot-installation.sh
+++ bin/chroot-installation.sh
@@ -49,24 +49,24 @@ cleanup_all() {
 }
 
 execute_ctmpfile() {
-	#set -x
+	set -x
 	chmod +x $CTMPFILE
-	sudo chroot $CHROOT_TARGET $TMPFILE
-	#set -o pipefail		# see eg http://petereisentraut.blogspot.com/2010/11/pipefail.html
-	#if ! $(sudo chroot $CHROOT_TARGET $TMPFILE 2>&1 | tee $TMPLOG) ; then
-	#	RESULT=$(egrep "Failed to fetch.*Unable to connect to" $TMPLOG || true)
-	#	if [ ! -z "$RESULT" ] ; then
-	#		echo
-	#		echo "Trying to workaround temporarily failure fetching packages, trying again..."
-	#		echo
-	#		sudo chroot $CHROOT_TARGET $TMPFILE
-	#	else
-	#		exit 1
-	#	fi
-	#fi
+	#sudo chroot $CHROOT_TARGET $TMPFILE
+	set -o pipefail		# see eg http://petereisentraut.blogspot.com/2010/11/pipefail.html
+	if ! $(sudo chroot $CHROOT_TARGET $TMPFILE 2>&1 | tee $TMPLOG) ; then
+		RESULT=$(egrep "Failed to fetch.*Unable to connect to" $TMPLOG || true)
+		if [ ! -z "$RESULT" ] ; then
+			echo
+			echo "Trying to workaround temporarily failure fetching packages, trying again..."
+			echo
+			sudo chroot $CHROOT_TARGET $TMPFILE
+		else
+			exit 1
+		fi
+	fi
 	rm $CTMPFILE
-	#set +o pipefail
-	#set +x
+	set +o pipefail
+	set +x
 	echo "Debug: This should only be printed on success."
 }
 

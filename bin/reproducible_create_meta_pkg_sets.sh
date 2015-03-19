3e9cbd126669c670efc149e53de91814a586ff7f
josch <j.schauer@email.de>
2015-03-19 13:42:18 +0100

    reproducible: shift build-essential-depends to position 4

--- bin/reproducible_create_meta_pkg_sets.sh
+++ bin/reproducible_create_meta_pkg_sets.sh
@@ -92,39 +92,39 @@ update_pkg_sets() {
 	fi
 
 	# popcon top 1337 installed sources
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[4]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[4]}.pkgset ] ; then
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[5]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[5]}.pkgset ] ; then
 		SQL_QUERY="SELECT popcon_src.source FROM popcon_src ORDER BY popcon_src.insts DESC LIMIT 1337;"
 		PGPASSWORD=public-udd-mirror \
 			psql -U public-udd-mirror \
 			-h public-udd-mirror.xvm.mit.edu -p 5432 \
 			-t \
 			udd -c"${SQL_QUERY}" > $TMPFILE
-		update_if_similar ${META_PKGSET[4]}.pkgset
+		update_if_similar ${META_PKGSET[5]}.pkgset
 	fi
 
 	# installed on one or more .debian.org machines
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[5]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[5]}.pkgset ] ; then
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[6]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[6]}.pkgset ] ; then
 		# one day we will get a proper data provider from DSA...
 		# (so far it was a manual "dpkg --get-selections" on all machines
 		# converted into a list of source packages...)
 		cat /srv/jenkins/bin/reproducible_installed_on_debian.org > $TMPFILE
-		update_if_similar ${META_PKGSET[5]}.pkgset
-	fi
-
-	# packages which had a DSA
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[6]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[6]}.pkgset ] ; then
-		svn export svn://svn.debian.org/svn/secure-testing/data/DSA/list ${TMPFILE2}
-		grep "^\[" ${TMPFILE2} | grep "DSA-" | cut -d " " -f5|sort -u > $TMPFILE
-		convert_into_source_packages_only
 		update_if_similar ${META_PKGSET[6]}.pkgset
 	fi
 
-	# gnome and everything it depends on
+	# packages which had a DSA
 	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[7]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[7]}.pkgset ] ; then
+		svn export svn://svn.debian.org/svn/secure-testing/data/DSA/list ${TMPFILE2}
+		grep "^\[" ${TMPFILE2} | grep "DSA-" | cut -d " " -f5|sort -u > $TMPFILE
+		convert_into_source_packages_only
+		update_if_similar ${META_PKGSET[7]}.pkgset
+	fi
+
+	# gnome and everything it depends on
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[8]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[8]}.pkgset ] ; then
 		chdist --data-dir=$CHPATH grep-dctrl-packages $DISTNAME -X \( -FPriority required --or -FPackage gnome \) > ${TMPFILE2}
 		schroot --directory /tmp -c source:jenkins-dpkg-jessie -- dose-deb-coinstall --deb-native-arch=$ARCH --bg=$PACKAGES --fg=${TMPFILE2} > $TMPFILE
 		convert_from_deb822_into_source_packages_only
-		update_if_similar ${META_PKGSET[7]}.pkgset
+		update_if_similar ${META_PKGSET[8]}.pkgset
 	fi
 
 	# The build-depends of X tasks can be solved once dose-ceve is able to read
@@ -138,38 +138,38 @@ update_pkg_sets() {
 
 	# all build depends of gnome
 	rm -f $TMPFILE
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[8]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[8]}.pkgset ] ; then
-		for PKG in $(cat $TPATH/${META_PKGSET[7]}.pkgset) ; do
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[9]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[9]}.pkgset ] ; then
+		for PKG in $(cat $TPATH/${META_PKGSET[8]}.pkgset) ; do
 			grep-dctrl -sBuild-Depends -n -X -FPackage $PKG $SOURCES | sed "s#([^()]*)##g ; s#\[[^][]*\]##g ; s#,##g" >> $TMPFILE
 		done
-		convert_into_source_packages_only
-		update_if_similar ${META_PKGSET[8]}.pkgset
-	fi
-
-	# tails
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[9]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[9]}.pkgset ] ; then
-		curl http://nightly.tails.boum.org/build_Tails_ISO_feature-jessie/latest.iso.binpkgs > $TMPFILE
-		curl http://nightly.tails.boum.org/build_Tails_ISO_feature-jessie/latest.iso.srcpkgs >> $TMPFILE
 		convert_into_source_packages_only
 		update_if_similar ${META_PKGSET[9]}.pkgset
 	fi
 
-	# all build depends of tails
-	rm -f $TMPFILE
-	if [ -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[10]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[10]}.pkgset ] ; then
-		for PKG in $(cat $TPATH/${META_PKGSET[9]}.pkgset) ; do
-			grep-dctrl -sBuild-Depends -n -X -FPackage $PKG $SOURCES | sed "s#([^()]*)##g ; s#\[[^][]*\]##g ; s#,##g" >> $TMPFILE
-		done
+	# tails
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[10]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[10]}.pkgset ] ; then
+		curl http://nightly.tails.boum.org/build_Tails_ISO_feature-jessie/latest.iso.binpkgs > $TMPFILE
+		curl http://nightly.tails.boum.org/build_Tails_ISO_feature-jessie/latest.iso.srcpkgs >> $TMPFILE
 		convert_into_source_packages_only
 		update_if_similar ${META_PKGSET[10]}.pkgset
 	fi
 
+	# all build depends of tails
+	rm -f $TMPFILE
+	if [ -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[11]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[11]}.pkgset ] ; then
+		for PKG in $(cat $TPATH/${META_PKGSET[10]}.pkgset) ; do
+			grep-dctrl -sBuild-Depends -n -X -FPackage $PKG $SOURCES | sed "s#([^()]*)##g ; s#\[[^][]*\]##g ; s#,##g" >> $TMPFILE
+		done
+		convert_into_source_packages_only
+		update_if_similar ${META_PKGSET[11]}.pkgset
+	fi
+
 	# grml
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[11]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[11]}.pkgset ] ; then
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[12]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[12]}.pkgset ] ; then
 		curl http://grml.org/files/grml64-full_latest/dpkg.selections | cut -f1 > $TMPFILE
 		if ! grep '<title>404 Not Found</title>' $TMPFILE ; then
 			convert_into_source_packages_only
-			update_if_similar ${META_PKGSET[11]}.pkgset
+			update_if_similar ${META_PKGSET[12]}.pkgset
 		else
 			echo "Warning: could not download grml's latest dpkg.selections file, skipping pkg set..."
 		fi
@@ -177,18 +177,18 @@ update_pkg_sets() {
 
 	# all build depends of grml
 	rm -f $TMPFILE
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[12]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[12]}.pkgset ] ; then
-		for PKG in $(cat $TPATH/${META_PKGSET[11]}.pkgset) ; do
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[13]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[13]}.pkgset ] ; then
+		for PKG in $(cat $TPATH/${META_PKGSET[12]}.pkgset) ; do
 			grep-dctrl -sBuild-Depends -n -X -FPackage $PKG $SOURCES | sed "s#([^()]*)##g ; s#\[[^][]*\]##g ; s#,##g" >> $TMPFILE
 		done
 		convert_into_source_packages_only
-		update_if_similar ${META_PKGSET[12]}.pkgset
+		update_if_similar ${META_PKGSET[13]}.pkgset
 	fi
 
 	# pkg-perl-maintainers
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[13]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[13]}.pkgset ] ; then
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[14]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[14]}.pkgset ] ; then
 		grep-dctrl -sPackage -n -FMaintainer pkg-perl-maintainers@lists.alioth.debian.org $SOURCES > $TMPFILE
-		update_if_similar ${META_PKGSET[13]}.pkgset
+		update_if_similar ${META_PKGSET[14]}.pkgset
 	fi
 
 	# build-essential-depends
@@ -210,13 +210,13 @@ update_pkg_sets() {
 	# trusted.
 	# So making this set reproducible is required to make anything
 	# in the essential or build-essential set trusted.
-	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[14]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[14]}.pkgset ] ; then
+	if [ ! -z $(find $TPATH -maxdepth 1 -mtime +0 -name ${META_PKGSET[4]}.pkgset) ] || [ ! -f $TPATH/${META_PKGSET[4]}.pkgset ] ; then
 		curl http://bootstrap.debian.net/importance_metric_all.txt > $TMPFILE2
 		# retrieve the highest number in the third column (packages affect)
 		HIGHEST=`sort -n -k 3 $TMPFILE2 | tail -1 | cut -f 3`
 		# now get all lines where the third column is equal to this value
 		awk '$3 == "'$HIGHEST'" { print $1 }' $TMPFILE2 | cut -d ':' -f 2 > $TMPFILE
-		update_if_similar ${META_PKGSET[14]}.pkgset
+		update_if_similar ${META_PKGSET[4]}.pkgset
 	fi
 }
 

2cc427ef8824dc65b019503dd9dd986c0e213633
Holger Levsen <holger@layer-acht.org>
2015-03-20 18:28:17 +0100

    reproducible: complain about unreproducible packages without debbindiff output. Thanks mapreri for the patch

--- bin/reproducible_html_all_packages.py
+++ bin/reproducible_html_all_packages.py
@@ -14,6 +14,20 @@
 from reproducible_common import *
 from reproducible_html_packages import gen_all_rb_pkg_pages
 
-
+# produce all packages html
 for suite in SUITES:
     gen_all_rb_pkg_pages(suite=suite)
+
+
+# now find those where debbindiff failed
+unreproducible = query_db('SELECT s.name, s.suite, s.architecture, r.version ' +
+                          'FROM sources AS s JOIN results AS r ON s.id=r.package_id ' +
+                          'WHERE r.status="unreproducible"')
+
+for pkg, suite, arch, version in unreproducible:
+    eversion = strip_epoch(version)
+    dbd = DBD_PATH + '/' + suite + '/' + arch + '/' + pkg + '_' + \
+          eversion + '.debbindiff.html'
+    if not os.access(dbd, os.R_OK):
+        log.critical(REPRODUCIBLE_URL + '/' + suite + '/' + arch + '/' + pkg +
+                     ' is unreproducible, yet it produced no debbindiff output.')

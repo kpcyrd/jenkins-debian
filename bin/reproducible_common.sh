ee2ad120d0e14fae72d194c574f142bcb1fd9893
Holger Levsen <holger@layer-acht.org>
2015-03-28 12:16:22 +0100

    reproducible: new meta-pkg set: xfce

--- bin/reproducible_common.sh
+++ bin/reproducible_common.sh
@@ -74,11 +74,13 @@ META_PKGSET[8]="gnome"
 META_PKGSET[9]="gnome_build-depends"
 META_PKGSET[10]="kde"
 META_PKGSET[11]="kde_build-depends"
-META_PKGSET[12]="tails"
-META_PKGSET[13]="tails_build-depends"
-META_PKGSET[14]="grml"
-META_PKGSET[15]="grml_build-depends"
-META_PKGSET[16]="maint_pkg-perl-maintainers"
+META_PKGSET[12]="xfce"
+META_PKGSET[13]="xfce_build-depends"
+META_PKGSET[14]="tails"
+META_PKGSET[15]="tails_build-depends"
+META_PKGSET[16]="grml"
+META_PKGSET[17]="grml_build-depends"
+META_PKGSET[18]="maint_pkg-perl-maintainers"
 
 schedule_packages() {
 	# these packages are manually scheduled, so should have high priority,

0aa7517b0c8f58b4fb68eac29a73def9fe447a12
Mattia Rizzolo <mattia@mapreri.org>
2015-04-06 18:14:00 +0200

    reproducible: setup_schroot: set up apt http_proxy in the chroot only if $http_proxy is defined

--- bin/reproducible_setup_schroot.sh
+++ bin/reproducible_setup_schroot.sh
@@ -47,7 +47,9 @@ bootstrap() {
 
 	echo -e '#!/bin/sh\nexit 101'              | sudo tee   $CHROOT_TARGET/usr/sbin/policy-rc.d >/dev/null
 	sudo chmod +x $CHROOT_TARGET/usr/sbin/policy-rc.d
-	echo "Acquire::http::Proxy \"$http_proxy\";" | sudo tee    $CHROOT_TARGET/etc/apt/apt.conf.d/80proxy >/dev/null
+	if [ ! -z "$http_proxy" ] ; then
+		echo "Acquire::http::Proxy \"$http_proxy\";" | sudo tee    $CHROOT_TARGET/etc/apt/apt.conf.d/80proxy >/dev/null
+	fi
 	echo "deb-src $MIRROR $SUITE main"        | sudo tee -a $CHROOT_TARGET/etc/apt/sources.list > /dev/null
 
 	sudo chroot $CHROOT_TARGET apt-get update

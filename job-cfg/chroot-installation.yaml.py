74ad08136faa03043e19e732b2629350fce25781
Holger Levsen <holger@layer-acht.org>
2015-04-15 12:16:42 +0200

    remove education-desktop-sugar related jobs from sid and jessie (see #782504)

--- job-cfg/chroot-installation.yaml.py
+++ job-cfg/chroot-installation.yaml.py
@@ -77,6 +77,9 @@ def get_targets_in_distro(distro, targets):
          # qt5, education-desktop-mate and cinnamon weren't in wheezy
          if distro == 'wheezy' and ( target == 'education-desktop-mate' or target == 'cinnamon' or target == 'qt5' ):
              continue
+         # sugar has been removed from jessie and thus education-desktop-sugar has been removed from jessie and sid
+         if (distro == 'sid' or distro == 'jessie') and ( target == 'education-desktop-sugar' ):
+             continue
          targets_in_distro.append(target)
      return targets_in_distro
 

ddff4a29a4bf55cd1e67df489ee9c838c82360cb
Holger Levsen <holger@layer-acht.org>
2015-04-28 16:59:55 +0200

    reproducible: schedule some more new versions

--- bin/reproducible_scheduler.py
+++ bin/reproducible_scheduler.py
@@ -241,11 +241,11 @@ def scheduler():
     # packages with new versions
     new = {}
     if total <= 100:
-        many_new = 100
+        many_new = 150
     elif total <= 200:
-        many_new = 75
+        many_new = 100
     else:
-        many_new = 50
+        many_new = 75
     log.info('Requesting ' + str(many_new) + ' new versions in ' + suite + '...')
     for suite in SUITES:
         new[suite] = scheduler_new_versions(suite, many_new)

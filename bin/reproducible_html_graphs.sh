9ef112a49a2d9bb6b81ca82714b8961804790ff9
Holger Levsen <holger@layer-acht.org>
2015-05-14 17:24:30 +0200

    reproducible: show number of uninvestigated packages

--- bin/reproducible_html_graphs.sh
+++ bin/reproducible_html_graphs.sh
@@ -550,8 +550,11 @@ create_main_stats_page() {
 	# write inventory table
 	write_page "<table class=\"main\"><tr><th>inventory type</th><th>amount</th></tr>"
 	write_page "<tr><td>packages with notes</td><td>$NOTES</td></tr>"
-	write_page "<tr><td>amount of issues in packages</td><td>$COUNT_ISSUES</td></tr>"
+	write_page "<tr><td>total amount of issues in packages</td><td>$COUNT_ISSUES</td></tr>"
 	write_page "<tr><td>issues categorized</td><td>$ISSUES</td></tr>"
+	SUITE="unstable"
+	gather_suite_stats
+	write_page "<tr><td>packages in $SUITE without notes</td><td>$(echo $COUNT_BAD + $COUNT_UGLY - $NOTES|bc)</td></tr>"
 	write_page "</table>"
 	# other graphs
 	write_page "<p>"

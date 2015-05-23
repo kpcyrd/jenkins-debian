762d062c14c193e8d251f0032af104de6c65d6bb
Holger Levsen <holger@layer-acht.org>
2015-05-23 21:35:13 +0200

    reproducible: fix count for number of packages with unidentied issues and provide link to index_no_notes.html

--- bin/reproducible_html_graphs.sh
+++ bin/reproducible_html_graphs.sh
@@ -558,7 +558,8 @@ create_main_stats_page() {
 	write_page "<tr><td>total amount of identified issues in packages</td><td>$COUNT_ISSUES</td></tr>"
 	SUITE="unstable"
 	gather_suite_stats
-	write_page "<tr><td>packages in $SUITE with issues but without any identified one</td><td>$(echo $COUNT_BAD + $COUNT_UGLY - $NOTES|bc)</td></tr>"
+	RESULT=$(sqlite3 -init ${INIT} -csv ${PACKAGES_DB} "SELECT COUNT(*) FROM (SELECT s.id FROM sources AS s JOIN results AS r ON r.package_id=s.id WHERE r.status IN ('unreproducible', 'FTBFS', 'blacklisted') AND s.id NOT IN (SELECT package_id FROM notes) AND s.suite='$SUITE' AND s.architecture='$ARCH')")
+	write_page "<tr><td>packages in $SUITE with issues but <a href=\"/$SUITE/$ARCH/index_no_notes.html\">without identified ones</a></td><td>$RESULT</td></tr>"
 	write_page "<tr><td>packages in $SUITE which need to be fixed</td><td>$(echo $COUNT_BAD + $COUNT_UGLY |bc) / $(echo $PERCENT_BAD + $PERCENT_UGLY|bc)%</td></tr>"
 	if [ -f ${NOTES_GIT_PATH}/packages.yml ] && [ -f ${NOTES_GIT_PATH}/issues.yml ] ; then
 		write_page "<tr><td>committers to <a href=\"https://anonscm.debian.org/cgit/reproducible/notes.git\" target=\"_parent\">notes.git</a> in total</td><td>$(cd ${NOTES_GIT_PATH} ; git log |grep Author|sort -u |wc -l)</td></tr>"

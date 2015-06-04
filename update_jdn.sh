48cdb95c75a208e4135db6cee356e490cb95d36f
Holger Levsen <holger@layer-acht.org>
2015-06-04 12:29:15 +0200

    thanks.html: mention Samuel and Lunar once, not twice

--- update_jdn.sh
+++ update_jdn.sh
@@ -245,7 +245,8 @@ cp -pr README INSTALL TODO CONTRIBUTING d-i-preseed-cfgs /var/lib/jenkins/userCo
 TMPFILE=$(mktemp)
 git log | grep ^Author| cut -d " " -f2-|sort -u > $TMPFILE
 echo "----" >> $TMPFILE
-cat THANKS.head $TMPFILE > /var/lib/jenkins/userContent/THANKS
+# samuel and lunar committed with several commiters, only display one
+grep -v "samuel.thibault@ens-lyon.org" THANKS.head | grep -v Lunar > $TMPFILE > /var/lib/jenkins/userContent/THANKS
 rm $TMPFILE
 cp -pr userContent /var/lib/jenkins/
 cd /var/lib/jenkins/userContent/

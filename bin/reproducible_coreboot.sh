e0a362d6a20f202573e368d149360ddbe387517b
Holger Levsen <holger@layer-acht.org>
2015-06-10 19:12:05 +0200

    reproducible coreboot: build with no payloads

--- bin/reproducible_coreboot.sh
+++ bin/reproducible_coreboot.sh
@@ -146,7 +146,7 @@ sed -i "s#cpus=1#cpus=$NUM_CPU#" util/abuild/abuild
 sed -i 's#USE_XARGS=1#USE_XARGS=0#g' util/abuild/abuild
 # actually build everything
 nice ionice -c 3 \
-	bash util/abuild/abuild || true # don't fail the full job just because some targets fail
+	bash util/abuild/abuild --payloads none || true # don't fail the full job just because some targets fail
 
 cd coreboot-builds
 for i in * ; do
@@ -175,7 +175,7 @@ NEW_NUM_CPU=$(echo $NUM_CPU-1|bc)
 sed -i "s#cpus=$NUM_CPU#cpus=$NEW_NUM_CPU#" util/abuild/abuild
 nice ionice -c 3 \
 	linux64 --uname-2.6 \
-	bash util/abuild/abuild || true # don't fail the full job just because some targets fail
+	bash util/abuild/abuild --payloads none || true # don't fail the full job just because some targets fail
 
 # reset environment to default values again
 export LANG="en_GB.UTF-8"
@@ -225,7 +225,7 @@ cat > $PAGE <<- EOF
 EOF
 write_page "       <h1>Reproducible Coreboot</h1>"
 write_page "       <p><em>Reproducible builds</em> enable anyone to reproduce bit by bit identical binary packages from a given source, so that anyone can verify that a given binary derived from the source it was said to be derived. There is a lot more information about <a href=\"https://wiki.debian.org/ReproducibleBuilds\">reproducible builds on the Debian wiki</a> and on <a href=\"https://reproducible.debian.net\">https://reproducible.debian.net</a>. The wiki has a lot more information, eg. why this is useful, what common issues exist and which workaround and solutions are known.<br />"
-write_page "        <em>Reproducible Coreboot</em> is an effort to apply this to coreboot. Thus each coreboot.rom is build twice, with a few varitations added and then those two ROMs are compared using <a href=\"https://tracker.debian.org/debbindiff\">debbindiff</a>. Please note that the toolchain is not varied at all as the rebuild happens on exactly the same system. More variations are expected to be seen in the wild.</p>"
+write_page "        <em>Reproducible Coreboot</em> is an effort to apply this to coreboot. Thus each coreboot.rom is build twice (without payloads), with a few varitations added and then those two ROMs are compared using <a href=\"https://tracker.debian.org/debbindiff\">debbindiff</a>. Please note that the toolchain is not varied at all as the rebuild happens on exactly the same system. More variations are expected to be seen in the wild.</p>"
 write_page "       <p>There is a monthly run <a href=\"https://jenkins.debian.net/view/reproducible/job/reproducible_coreboot/\">jenkins job</a> to test the <code>master</code> branch of <a href=\"https://review.coreboot.org/p/coreboot.git\">coreboot.git</a>. Currently this job is triggered more often though, because this is still under development and brand new. The jenkins job is simply running <a href=\"http://anonscm.debian.org/cgit/qa/jenkins.debian.net.git/tree/bin/reproducible_coreboot.sh\">reproducible_coreboot.sh</a> in a Debian environemnt and this script is solely responsible for creating this page. Feel invited to join <code>#debian-reproducible</code> (on irc.oftc.net) to request job runs whenever sensible. Patches and other <a href=\"reproducible-builds@lists.alioth.debian.org\">feedback</a> are very much appreciated!</p>"
 write_page "<p><pre>"
 echo -n "$COREBOOT" >> $PAGE

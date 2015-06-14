bf3f5366dd463cdcc8b4ccd50e434e7195bba742
Mattia Rizzolo <mattia@mapreri.org>
2015-06-14 20:14:07 +0200

    reproducible: schedule_on_demand: rewrite it to just be a wrapper around reproducible_remote_scheduler.py

--- bin/reproducible_schedule_on_demand.sh
+++ bin/reproducible_schedule_on_demand.sh
@@ -4,45 +4,16 @@
 #         © 2015 Mattia Rizzolo <mattia@mapreri.org>
 # released under the GPLv=2
 
-DEBUG=false
-. /srv/jenkins/bin/common-functions.sh
-common_init "$@"
-
-# common code defining db access
-. /srv/jenkins/bin/reproducible_common.sh
-
-set +x
-ARTIFACTS=0
-NOTIFY=''
-if [ "$1" = "--notify" ] ; then
-	NOTIFY=true
-	shift
-elif [ "$1" = "--artifacts" ] ; then
-	ARTIFACTS=1
-	NOTIFY=true
-fi
-SUITE="$1"
-shift
-if [ "$SUITE" = "sid" ] ; then
-	echo "WARNING: sid has been renamed to unstable."
-	SUITE=unstable
-fi
+set -e
 
 if [ ! -z "$SUDO_USER" ] ; then
 	REQUESTER="$SUDO_USER"
 else
 	echo "Looks like you logged into this host as the jenkins user without sudoing to it. How can that be possible?!?!"
-	REQUESTER="$USER"
-fi
-
-CANDIDATES="$@"
-if [ ${#} -gt 50 ] && [ "$NOTIFY" = "true" ] ; then
-	echo
-	figlet "No."
-	echo "Do not schedule more than 50 packages with notification. If you really really need to spam the IRC channel this much, use a loop to achieve that. Exiting."
-	echo
+	echo "You're doing something too weird to be supported, please be normal, exiting."
 	exit 1
 fi
 
-# finally
-schedule_packages $CANDIDATES
+LC_USER="$REQUESTER" \
+LOCAL_CALL="true" \
+/srv/jenkins/bin/reproducible_remote_scheduler.py "$@"

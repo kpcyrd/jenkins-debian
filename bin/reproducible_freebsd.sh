d38f9c7067b9b41cde8815e24925156ba9e3109c
Holger Levsen <holger@layer-acht.org>
2015-07-18 05:14:39 +0200

    reproducible freebsd: cleanup properly

--- bin/reproducible_freebsd.sh
+++ bin/reproducible_freebsd.sh
@@ -18,8 +18,8 @@ MACHINES="sparc64 amd64"
 
 cleanup_tmpdirs() {
 	cd
-	rm -r $TMPDIR
-	rm -r $TMPBUILDDIR
+	$RSSH "rm -r $TMPDIR"
+	$RSSH "rm -r $TMPBUILDDIR"
 }
 
 create_results_dirs() {
@@ -49,8 +49,8 @@ echo "==========================================================================
 echo "$(date -u) - Cloning the freebsd git repository (which is autosynced with their CVS repository)"
 echo "============================================================================="
 $RSSH git clone https://github.com/freebsd/freebsd.git $TMPBUILDDIR/freebsd
-FREEBSD="$($RSSH \"cd $TMPBUILDDIR/freebsd ; git log -1\")"
-FREEBSD_VERSION="$($RSSH \"cd $TMPBUILDDIR/freebsd ; git describe --always\")"
+FREEBSD=$($RSSH "cd $TMPBUILDDIR/freebsd ; git log -1")
+FREEBSD_VERSION=$($RSSH "cd $TMPBUILDDIR/freebsd ; git describe --always")
 echo "This is freebsd $FREEBSD_VERSION."
 echo
 $RSSH "cd $TMPBUILDDIR/freebsd ; git log -1"

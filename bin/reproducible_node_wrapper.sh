a73f8416a63374173ea3572501eb054149b3e526
Holger Levsen <holger@layer-acht.org>
2015-08-06 17:56:26 +0200

    reproducible: also created new schroots for downloading sources on bpi0, cbxi4pro0, hb0, wbq0 and profitbricks1

--- bin/reproducible_node_wrapper.sh
+++ bin/reproducible_node_wrapper.sh
@@ -65,24 +65,32 @@ elif [ "$*" = "reproducible_maintenance_armhf_bpi0" ] ; then
 	exec /srv/jenkins/bin/reproducible_maintenance.sh ; croak "Exec failed";
 elif [ "$*" = "reproducible_setup_schroot_testing_debbindiff_armhf_bpi0" ] ; then
 	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing-debbindiff testing debbindiff locales-all ; croak "Exec failed";
+elif [ "$*" = "reproducible_setup_schroot_testing_armhf_bpi0" ] ; then
+	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing testing ; croak "Exec failed";
 elif [ "$*" = "reproducible_setup_pbuilder_testing_armhf_cbxi4pro0" ] ; then
 	exec /srv/jenkins/bin/reproducible_setup_pbuilder.sh testing ; croak "Exec failed";
 elif [ "$*" = "reproducible_maintenance_armhf_cbxi4pro0" ] ; then
 	exec /srv/jenkins/bin/reproducible_maintenance.sh ; croak "Exec failed";
 elif [ "$*" = "reproducible_setup_schroot_testing_debbindiff_armhf_cbxi4pro0" ] ; then
 	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing-debbindiff testing debbindiff locales-all ; croak "Exec failed";
+elif [ "$*" = "reproducible_setup_schroot_testing_armhf_cbxi4pro0" ] ; then
+	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing testing ; croak "Exec failed";
 elif   [ "$*" = "reproducible_setup_pbuilder_testing_armhf_hb0" ] ; then
 	exec /srv/jenkins/bin/reproducible_setup_pbuilder.sh testing ; croak "Exec failed";
 elif [ "$*" = "reproducible_maintenance_armhf_hb0" ] ; then
 	exec /srv/jenkins/bin/reproducible_maintenance.sh ; croak "Exec failed";
 elif [ "$*" = "reproducible_setup_schroot_testing_debbindiff_armhf_hb0" ] ; then
 	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing-debbindiff testing debbindiff locales-all ; croak "Exec failed";
+elif [ "$*" = "reproducible_setup_schroot_testing_armhf_hb0" ] ; then
+	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing testing ; croak "Exec failed";
 elif   [ "$*" = "reproducible_setup_pbuilder_testing_armhf_wbq0" ] ; then
 	exec /srv/jenkins/bin/reproducible_setup_pbuilder.sh testing ; croak "Exec failed";
 elif [ "$*" = "reproducible_maintenance_armhf_wbq0" ] ; then
 	exec /srv/jenkins/bin/reproducible_maintenance.sh ; croak "Exec failed";
 elif [ "$*" = "reproducible_setup_schroot_testing_debbindiff_armhf_wbq0" ] ; then
 	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing-debbindiff testing debbindiff locales-all ; croak "Exec failed";
+elif [ "$*" = "reproducible_setup_schroot_testing_armhf_wbq0" ] ; then
+	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing testing ; croak "Exec failed";
 elif   [ "$*" = "reproducible_setup_pbuilder_testing_amd64_profitbricks1" ] ; then
 	exec /srv/jenkins/bin/reproducible_setup_pbuilder.sh testing ; croak "Exec failed";
 elif   [ "$*" = "reproducible_setup_pbuilder_unstable_amd64_profitbricks1" ] ; then
@@ -95,6 +103,12 @@ elif [ "$*" = "reproducible_setup_schroot_testing_debbindiff_amd64_profitbricks1
 	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing-debbindiff testing debbindiff locales-all ; croak "Exec failed";
 elif [ "$*" = "reproducible_setup_schroot_unstable_debbindiff_amd64_profitbricks1" ] ; then
 	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-unstable-debbindiff unstable debbindiff locales-all ; croak "Exec failed";
+elif [ "$*" = "reproducible_setup_schroot_testing_amd64_profitbricks1" ] ; then
+	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-testing testing ; croak "Exec failed";
+elif [ "$*" = "reproducible_setup_schroot_unstable_amd64_profitbricks1" ] ; then
+	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-unstable unstable ; croak "Exec failed";
+elif [ "$*" = "reproducible_setup_schroot_experimental_amd64_profitbricks1" ] ; then
+	exec /srv/jenkins/bin/schroot-create.sh reproducible reproducible-experimental experimental ; croak "Exec failed";
 fi
 
 croak "Command '$*' not found in allowed commands."

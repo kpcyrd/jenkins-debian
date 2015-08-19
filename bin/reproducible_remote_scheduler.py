b033ad579894fb9178c9d6690a246d433d767639
Mattia Rizzolo <mattia@mapreri.org>
2015-08-19 21:11:52 +0000

    reproducible: remote_scheduler: rearrange the argparse bits to really print all option in -h

--- bin/reproducible_remote_scheduler.py
+++ bin/reproducible_remote_scheduler.py
@@ -21,16 +21,9 @@ parser = argparse.ArgumentParser(
            ' issue AND that status AND that date". Blacklisted package '
            "can't be selected by a filter, but needs to be explitely listed"
            ' in the package list.')
+parser.add_argument('--dry-run', action='store_true')
 parser.add_argument('--null', action='store_true', help='The arguments are '
                     'considered null-separated and coming from stding')
-
-null_args = parser.parse_known_args()[0]
-if null_args.null:
-    input_args = sys.stdin.read().split('\0')
-else:
-    input_args = sys.argv
-
-parser.add_argument('--dry-run', action='store_true')
 parser.add_argument('-k', '--keep-artifacts',  action='store_true',
                    help='Save artifacts (for further offline study)')
 parser.add_argument('-n', '--notify', action='store_true',
@@ -55,7 +48,9 @@ parser.add_argument('-s', '--suite', required=False,
                     help='Specify the suite to schedule in')
 parser.add_argument('packages', metavar='package', nargs='*',
                     help='list of packages to reschedule')
-scheduling_args = parser.parse_known_args(args=input_args)[0]
+scheduling_args = parser.parse_known_args()[0]
+if scheduling_args.null:
+    scheduling_args = parser.parse_lmown_args(sys.stdin.read().split('\0'))
 
 # these are here as an hack to be able to parse the command line
 from reproducible_common import *

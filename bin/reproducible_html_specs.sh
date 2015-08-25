97f15251c262dac6817f740f7448b6c07a948fa0
Holger Levsen <holger@layer-acht.org>
2015-08-25 12:54:16 +0200

    reproducible: new job to build /specs/source_date_epoch/ from source-date-epoch-spec.git

---
+++ bin/reproducible_html_specs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright © 2015 Mattia Rizzolo <mattia@mapreri.org>
+# Copyright © 2015 Holger Levsen <holger@mapreri.org>
+# released under the GPLv=2
+
+DEBUG=false
+. /srv/jenkins/bin/common-functions.sh
+common_init "$@"
+
+. /srv/jenkins/bin/reproducible_common.sh
+
+set -e
+
+VERSION=$(git log -1 --pretty='%h')
+SPEC=$1
+
+make html
+
+mkdir -pv "$BASE/specs/$1"
+
+mv -v html/* "$BASE/specs/$1"
+
+irc_message "$REPRODUCIBLE_URL/specs/$1 updated to $VERSION"
+
+

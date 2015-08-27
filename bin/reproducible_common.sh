fe5e08ac81bf275f1dc36fb33832e92b8940dd36
Holger Levsen <holger@layer-acht.org>
2015-08-27 15:17:10 +0200

    reproducible: add commas as suggested by Faux

--- bin/reproducible_common.sh
+++ bin/reproducible_common.sh
@@ -182,8 +182,8 @@ write_page_header() {
 		write_page " Thanks to <a href=\"https://www.profitbricks.co.uk\">Profitbricks</a> for donating the virtual machines this is running on!</p>"
 	fi
 	if [ "$1" = "dd-list" ] || [ "$1" = "stats" ] ; then
-		write_page "<p>Join <code>#debian-reproducible</code> on OFTC"
-		write_page "   or <a href="mailto:reproducible-builds@lists.alioth.debian.org">send us an email</a>"
+		write_page "<p>Join <code>#debian-reproducible</code> on OFTC,"
+		write_page "   or <a href="mailto:reproducible-builds@lists.alioth.debian.org">send us an email</a>,"
 		write_page "   to get support for making sure your packages build reproducibly too. Also, we care about free software in general,"
 		write_page "   so if you are an upstream developer or working on another distribution, we'd love to hear from you!"
 		write_page "   Besides Debian we are also testing <a href=\"/coreboot/\">coreboot</a>, <a href=\"/openwrt/\">OpenWrt</a>, <a href=\"$JENKINS_URL/userContent/todo.html#_reproducible_netbsd\">NetBSD</a> and <a href=\"/freebsd/\">FreeBSD</a> now, though not as thoroughly as Debian (yet?) - and there are plans to test <a href=\"$JENKINS_URL/userContent/todo.html#_reproducible_fedora\">Fedora</a> too."

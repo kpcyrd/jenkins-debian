4af00c04a9041fef093ca941f5cb5a83bc2db189
Holger Levsen <holger@layer-acht.org>
2015-08-27 17:23:57 +0200

    reproducible: fix link target

--- bin/reproducible_common.py
+++ bin/reproducible_common.py
@@ -273,7 +273,7 @@ def _gen_links(suite, arch):
     for i in SUITES:  # suite links
         if arch == 'armhf' and i != 'unstable':
             continue
-        html += '<li><a href="/' + i + 'index_suite_' + arch + '_stats.html">suite: ' + i + '</a></li>'
+        html += '<li><a href="/' + i + '/index_suite_' + arch + '_stats.html">suite: ' + i + '</a></li>'
     if arch == 'amd64':
         html += '<li><a href="/unstable/index_suite_armhf_stats.html\">arch: armhf</a></li>'
     else:

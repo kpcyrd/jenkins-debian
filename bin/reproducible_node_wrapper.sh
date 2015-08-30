75bab5314f12a03dfe4663ce1bfd1ab50534a895
Holger Levsen <holger@layer-acht.org>
2015-08-30 20:28:31 +0200

    reproducible: allow rebootstrap jobs on nodes too

--- bin/reproducible_node_wrapper.sh
+++ bin/reproducible_node_wrapper.sh
@@ -59,7 +59,11 @@ info "remote_host called with $*"
 
 allowed_cmds=()
 
-if [ "$1" = "/srv/jenkins/bin/reproducible_build.sh" ] && ( [ "$2" = "1" ] || [ "$2" = "2" ] ) ; then
+if [ "$1" = "/srv/jenkins/bin/chroot-run.sh" ] && [ "$2" = "sid" ] && [ "$3" = "minimal" ] && [ "$4" = "./bootstrap.sh" ] ; then
+	shift
+	export LC_ALL=C
+	exec /srv/jenkins/bin/chroot-run.sh $@ ; croak "Exec failed";
+elif [ "$1" = "/srv/jenkins/bin/reproducible_build.sh" ] && ( [ "$2" = "1" ] || [ "$2" = "2" ] ) ; then
 	exec /srv/jenkins/bin/reproducible_build.sh $2 $3 $4 ; croak "Exec failed";
 elif   [ "$*" = "reproducible_setup_pbuilder_unstable_armhf_bpi0" ] ; then
 	exec /srv/jenkins/bin/reproducible_setup_pbuilder.sh unstable ; croak "Exec failed";

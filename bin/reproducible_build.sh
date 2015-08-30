a12595877ffcc74914a8d7ca6783a703942ec891
Holger Levsen <holger@layer-acht.org>
2015-08-30 18:41:57 +0200

    reproducible: never try to generate html on the build nodes

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -96,9 +96,11 @@ cleanup_all() {
 		irc_message "$REPRODUCIBLE_URL/$SUITE/$ARCH/$SRCPACKAGE done: $STATUS"
 	fi
 	gzip -9fvn $RBUILDLOG
-	# XXX quite ugly: this is just needed to get the correct value of the
-	# compressed files in the html. It's cheap and quite safe so, *shrugs*...
-	gen_package_html $SRCPACKAGE
+	if [ "$MODE" = "legacy" ] || [ "$MODE" = "ng" ] ; then
+		# XXX quite ugly: this is just needed to get the correct value of the
+		# compressed files in the html. It's cheap and quite safe so, *shrugs*...
+		gen_package_html $SRCPACKAGE
+	fi
 	cd
 	rm -r $TMPDIR
 	if ! $BAD_LOCKFILE ; then rm -f $LOCKFILE ; fi

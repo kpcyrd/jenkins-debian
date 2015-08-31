5eedbfb0a572735159bc296aa0a61e8a98bee4e4
Holger Levsen <holger@layer-acht.org>
2015-08-31 03:01:42 +0200

    reproducible: try harder to fixup that rsync…

--- bin/jenkins_node_wrapper.sh
+++ bin/jenkins_node_wrapper.sh
@@ -70,7 +70,7 @@ if [[ "$*" =~ rebootstrap_.* ]] ; then
 elif [ "$1" = "/srv/jenkins/bin/reproducible_build.sh" ] && ( [ "$2" = "1" ] || [ "$2" = "2" ] ) ; then
 	exec /srv/jenkins/bin/reproducible_build.sh $2 $3 $4 $5 ; croak "Exec failed";
 elif [[ "$*" =~ rsync\ --server\ --sender\ .*\ .\ /srv/reproducible-results/tmp.* ]] ; then
-	exec rsync --server --sender .* . $6 ; croak "Exec failed";
+	exec rsync --server --sender $4 . $6 ; croak "Exec failed";
 elif [[ "$*" =~ rm\ -r\ /srv/reproducible-results/tmp.* ]] ; then
 	exec rm -r $3 ; croak "Exec failed";
 elif   [ "$*" = "reproducible_setup_pbuilder_unstable_armhf_bpi0" ] ; then

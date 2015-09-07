e29f37ccffb733d4d2dd1e271fb92a7b0af99144
Holger Levsen <holger@layer-acht.org>
2015-09-08 00:06:28 +0200

    reproducible: fixup html+css

--- bin/reproducible_html_dashboards.sh
+++ bin/reproducible_html_dashboards.sh
@@ -424,9 +424,10 @@ create_main_stats_page() {
 	# other archs: armhf
 	ARCH="armhf"
 	SUITE="unstable"
+	write_page "</p><p style=\"clear:both;\">"
 	write_page " <hr />"
 	write_page " <a href=\"/$SUITE/index_suite_${ARCH}_stats.html\"><img src=\"/$SUITE/$ARCH/${TABLE[0]}.png\" class=\"overview\" alt=\"$SUITE/$ARCH stats\"></a>"
-	write_page " <a href=\"/${TABLE[1]}_$ARCH.png\"><img src=\"/${TABLE[1]}_$ARCH.png\" alt=\"${MAINLABEL[$i]}\"></a>"
+	write_page " <a href=\"/${TABLE[1]}_$ARCH.png\"><img src=\"/${TABLE[1]}_$ARCH.png\" class=\"overview\" alt=\"${MAINLABEL[$i]}\"></a>"
 	write_page " <a href=\"/$SUITE/$ARCH/${TABLE[2]}.png\"><img src=\"/$SUITE/$ARCH/${TABLE[2]}.png\" class=\"overview\" alt=\"age of oldest reproducible build result in $SUITE/$ARCH\"></a>"
 	write_build_performace_stats
 	write_page " <hr />"

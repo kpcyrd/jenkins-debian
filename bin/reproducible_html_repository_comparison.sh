1edfffa4ee04eb94eed24e535f2a775516634332
Holger Levsen <holger@layer-acht.org>
2015-09-09 11:58:05 +0200

    reproducible: fix navigation

--- bin/reproducible_html_repository_comparison.sh
+++ bin/reproducible_html_repository_comparison.sh
@@ -9,7 +9,8 @@ common_init "$@"
 
 # common code defining db access
 . /srv/jenkins/bin/reproducible_common.sh
-# temporarily overwrite defaults...
+ 
+# overwrite defaults as we need this order here
 ARCHS="amd64 armhf"
 
 VIEW=repositories
@@ -28,6 +29,7 @@ write_row() {
         echo "$1" >> $ROW
 }
 
+ARCH="amd64"
 echo "$(date) - starting to write $PAGE page."
 write_page_header $VIEW "Comparison between the reproducible builds apt repository and regular Debian suites"
 write_page "<p>These source packages are different from unstable in our apt repository on alioth. They are available for <a href=\"https://wiki.debian.org/ReproducibleBuilds/ExperimentalToolchain#Usage_example\">testing using these sources.lists</a> entries:<pre>"

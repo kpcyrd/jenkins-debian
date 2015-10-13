a4f856993e3c62c4a5afdd42f9a9531169e97287
Holger Levsen <holger@layer-acht.org>
2015-10-13 14:10:23 +0200

    reproducible: install more build dependencies for coreboot, openwrt and netbsd

--- update_jdn.sh
+++ update_jdn.sh
@@ -151,7 +151,7 @@ if [ -f /etc/debian_version ] ; then
 		esac
 		# needed to run coreboot/openwrt/netbsd jobs
 		case $HOSTNAME in
-			profitbricks-build3-amd64) DEBS="$DEBS m4 bison flex" ;;
+			profitbricks-build3-amd64) DEBS="$DEBS bison cmake diffutils findutils flex g++ gawk gcc git grep iasl libc6-dev m4 make subversion unzip util-linux zlib1g-dev" ;;
 			*) ;;
 		esac
 		if [ "$HOSTNAME" = "jenkins" ] ; then

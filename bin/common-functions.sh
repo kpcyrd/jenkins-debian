5cfc4ced688642da7363c7bceef52eeda35b6b56
Holger Levsen <holger@layer-acht.org>
2015-10-17 17:23:40 +0200

    reproducible: always be true

--- bin/common-functions.sh
+++ bin/common-functions.sh
@@ -137,14 +137,14 @@ cleanup_schroot_sessions() {
 	for loop in $(seq 0 40) ; do
 		# first, check if no process using "schroot" is running, if thats the case, loop through all schroot sessions:
 		# arch sessions are ignored, because they are handled properly
-		pgrep -f "schroot --directory" || for i in $(schroot --all-sessions -l |grep -v "session:arch") ; do
+		pgrep -f "schroot --directory" || for i in $(schroot --all-sessions -l |grep -v "session:arch"||true) ; do
 			# then, check that schroot is still not run, and then delete the session
 			if [ -z $i ] ; then
 				continue
 			fi
 			pgrep -f "schroot --directory" || schroot -e -c $i
 		done
-		RESULT=$(schroot --all-sessions -l|grep -v "session:arch")
+		RESULT=$(schroot --all-sessions -l|grep -v "session:arch"||true)
 		if [ -z "$RESULT" ] ; then
 			echo "No schroot sessions in use atm..."
 			echo

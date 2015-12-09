2a31967b96677472252d66bd54cc1e5b743c7a0d
Holger Levsen <holger@layer-acht.org>
2015-12-09 11:09:50 +0100

    reproducible archlinux: refactor html

--- bin/reproducible_html_archlinux.sh
+++ bin/reproducible_html_archlinux.sh
@@ -41,31 +41,31 @@ write_page "    <table><tr><th>source package</th><th>test result</th><th>test d
 for PKG in $(find $ARCHBASE/* -maxdepth 1 -type d -exec basename {} \;) ; do
 	write_page "     <tr>"
 	write_page "      <td>$PKG</td>"
+	write_page "      <td>"
 	if [ -z "$(cd $ARCHBASE/$PKG/ ; ls *.pkg.tar.xz.html 2>/dev/null)" ] ; then
 		if [ ! -z "$(grep '==> ERROR: Could not resolve all dependencies' $ARCHBASE/$PKG/build1.log)" ] ; then
-			write_page "      <td>could not resolve dependencies</td>"
+			write_page "       could not resolve dependencies"
 		elif [ ! -z "$(egrep '==> ERROR: .pacman. failed to install missing dependencies.' $ARCHBASE/$PKG/build1.log)" ] ; then
-			write_page "      <td>failed to install dependencies</td>"
+			write_page "       failed to install dependencies"
 		elif [ ! -z "$(egrep '==> ERROR: A failure occurred in (build|package)' $ARCHBASE/$PKG/build1.log)" ] ; then
-			write_page "      <td>failed to build from source</td>"
+			write_page "       failed to build from source"
 		elif [ ! -z "$(egrep '==> ERROR: A failure occurred in check' $ARCHBASE/$PKG/build1.log)" ] ; then
-			write_page "      <td>failed to build from source, while running tests</td>"
+			write_page "       failed to build from source, while running tests"
 		elif [ ! -z "$(egrep '==> ERROR: Failure while downloading' $ARCHBASE/$PKG/build1.log)" ] ; then
-			write_page "      <td>failed to download source</td>"
+			write_page "       failed to download source"
 		elif [ ! -z "$(egrep '==> ERROR: One or more files did not pass the validity check' $ARCHBASE/$PKG/build1.log)" ] ; then
-			write_page "      <td>failed to verify source</td>"
+			write_page "       failed to verify source"
 		elif [ ! -z "$(egrep 'makepkg was killed by timeout after 4h' $ARCHBASE/$PKG/build1.log)" ] ; then
-			write_page "      <td>failed to build, killed by timeout after 4h</td>"
+			write_page "       failed to build, killed by timeout after 4h"
 		else
-			write_page "      <td>probably failed to build from source, please investigate</td>"
+			write_page "       probably failed to build from source, please investigate"
 		fi
 	else
-		write_page "      <td>"
 		for ARTIFACT in $(cd $ARCHBASE/$PKG/ ; ls *.pkg.tar.xz.html) ; do
-			write_page "   <a href=\"/archlinux/$PKG/$ARTIFACT\">${ARTIFACT:0:-5}</a>is unreproducible<br />"
+			write_page "       <a href=\"/archlinux/$PKG/$ARTIFACT\">${ARTIFACT:0:-5}</a>is unreproducible<br />"
 		done
-		write_page "      </td>"
 	fi
+	write_page "      </td>"
 	write_page "      <td>$(ls -dl $ARCHBASE/$PKG/build1.log|cut -d " " -f6-8)</td>"
 	for LOG in build1.log build2.log ; do
 		if [ -f $ARCHBASE/$PKG/$LOG ] ; then

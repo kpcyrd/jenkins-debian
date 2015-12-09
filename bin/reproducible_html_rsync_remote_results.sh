e24a1d6d89322ac01d7109b75863bf6165928460
Holger Levsen <holger@layer-acht.org>
2015-12-09 10:53:29 +0100

    reproducible coreboot/openwrt/netbsd: preserve images and css when updating the results

--- bin/reproducible_html_rsync_remote_results.sh
+++ bin/reproducible_html_rsync_remote_results.sh
@@ -15,10 +15,16 @@ rsync_remote_results() {
 	for PROJECT in coreboot openwrt netbsd ; do
 		echo "$(date -u) - Starting to rsync results for '$PROJECT'."
 		local RESULTS=$(mktemp --tmpdir=$TEMPDIR -d reproducible-rsync-XXXXXXXXX)
+		# copy the new results from build node to webserver node
 		rsync -r -v -e ssh profitbricks-build3-amd64.debian.net:$BASE/$PROJECT/ $RESULTS
-		mv $BASE/$PROJECT ${RESULTS}.tmp
 		chmod 775 $RESULTS
+		# move old results out of the way
+		mv $BASE/$PROJECT ${RESULTS}.tmp
+		# preserve images and css
+		find ${RESULTS}.tmp -name "*css" -o -name "*png" -o -name "*jpg" -exec cp -v {} $RESULTS/ \;
+		# make the new results visible
 		mv $RESULTS $BASE/$PROJECT
+		# delete the old results
 		rm ${RESULTS}.tmp -r
 		echo "$(date -u) - $REPRODUCIBLE_URL/$PROJECT has been updated."
 	done

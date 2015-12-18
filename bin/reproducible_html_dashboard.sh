232a69684d7ca76d13a66990f7e72c0e4f62e6cb
Holger Levsen <holger@layer-acht.org>
2015-12-18 22:01:50 +0100

    reproducible debian armhf: order graphs differently

--- bin/reproducible_html_dashboard.sh
+++ bin/reproducible_html_dashboard.sh
@@ -496,7 +496,9 @@ create_dashboard_page() {
 	write_suite_table
 	for SUITE in unstable experimental ; do
 		write_page " <a href=\"/$SUITE/index_suite_${ARCH}_stats.html\"><img src=\"/$SUITE/$ARCH/${TABLE[0]}.png\" class=\"halfview\" alt=\"$SUITE/$ARCH stats\"></a>"
-		write_page " <a href=\"/$SUITE/$ARCH/${TABLE[2]}.png\"><img src=\"/$SUITE/$ARCH/${TABLE[2]}.png\" class=\"halfview\" alt=\"age of oldest reproducible build result in $SUITE/$ARCH\"></a>"
+	done
+	for SUITE in unstable experimental ; do
+		write_page " <a href=\"/$SUITE/$ARCH/${TABLE[2]}.png\"><img src=\"/$SUITE/$ARCH/${TABLE[2]}.png\" class=\"overview\" alt=\"age of oldest reproducible build result in $SUITE/$ARCH\"></a>"
 	done
 	write_page " <a href=\"/${TABLE[1]}_$ARCH.png\"><img src=\"/${TABLE[1]}_$ARCH.png\" class=\"overview\" alt=\"${MAINLABEL[$i]}\"></a>"
 	write_build_performance_stats

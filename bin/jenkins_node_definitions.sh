a77ecd79d9468cdcb851a7222905e03e3bf1a990
Holger Levsen <holger@layer-acht.org>
2016-02-03 02:28:37 +0100

    reproducible armhf: add new armhf node, thanks to Vagrant

--- bin/jenkins_node_definitions.sh
+++ bin/jenkins_node_definitions.sh
@@ -4,7 +4,7 @@
 # released under the GPLv=2
 
 # define Debian build nodes in use
-BUILD_NODES="profitbricks-build1-amd64.debian.net profitbricks-build2-amd64.debian.net profitbricks-build5-amd64.debian.net profitbricks-build6-amd64.debian.net wbq0-armhf-rb.debian.net cbxi4pro0-armhf-rb.debian.net bpi0-armhf-rb.debian.net hb0-armhf-rb.debian.net odxu4-armhf-rb.debian.net wbd0-armhf-rb.debian.net rpi2b-armhf-rb.debian.net rpi2c-armhf-rb.debian.net odxu4b-armhf-rb.debian.net odxu4c-armhf-rb.debian.net ff2a-armhf-rb.debian.net ff2b-armhf-rb.debian.net opi2a-armhf-rb.debian.net opi2b-armhf-rb.debian.net"
+BUILD_NODES="profitbricks-build1-amd64.debian.net profitbricks-build2-amd64.debian.net profitbricks-build5-amd64.debian.net profitbricks-build6-amd64.debian.net wbq0-armhf-rb.debian.net cbxi4a-armhf-rb.debian.net cbxi4pro0-armhf-rb.debian.net bpi0-armhf-rb.debian.net hb0-armhf-rb.debian.net odxu4-armhf-rb.debian.net wbd0-armhf-rb.debian.net rpi2b-armhf-rb.debian.net rpi2c-armhf-rb.debian.net odxu4b-armhf-rb.debian.net odxu4c-armhf-rb.debian.net ff2a-armhf-rb.debian.net ff2b-armhf-rb.debian.net opi2a-armhf-rb.debian.net opi2b-armhf-rb.debian.net"
 
 # return the ports sshd is listening on
 get_node_ssh_port() {
@@ -18,6 +18,9 @@ get_node_ssh_port() {
 	    ;;
 	  wbq0-armhf-rb.debian.net)
 	    PORT=2225
+	    ;;
+	  cbxi4a-armhf-rb.debian.net)
+	    PORT=2239
 	    ;;
 	  cbxi4pro0-armhf-rb.debian.net)
 	    PORT=2226

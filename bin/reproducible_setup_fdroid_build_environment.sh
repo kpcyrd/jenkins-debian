1596171d45ccc7196b8b9a62bf44e5c72483dc02
Hans-Christoph Steiner <hans@eds.org>
2016-02-09 13:33:44 +0100

    reproducible fdroid: this script is now run directly in $WORKSPACE

--- bin/reproducible_setup_fdroid_build_environment.sh
+++ bin/reproducible_setup_fdroid_build_environment.sh
@@ -3,26 +3,12 @@
 # Copyright 2015-2016 Holger Levsen <holger@layer-acht.org>
 # released under the GPLv=2
 
-#
-#
-
 DEBUG=true
 . /srv/jenkins/bin/common-functions.sh
 common_init "$@"
 
 # common code
 . /srv/jenkins/bin/reproducible_common.sh
-
-# define and clean work space (differently than jenkins would normally do as we run via ssh on a different node…)
-WORKSPACE=$BASE/fdroid
-
-# TODO:
-#
-# add locking here to only run this if no build job is running…
-
-# fdroidserver.git/jenkins-build-makebuildserver assumes $WORKSPACE is
-# the root of fdroidserver.git/
-cd $WORKSPACE
 
 # this script is maintained upstream and is also run on Guardian
 # Project's jenkins box

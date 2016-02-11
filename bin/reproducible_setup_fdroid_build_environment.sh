9e6877911785345ff48a149c555bb77c2cd9ecd0
Holger Levsen <holger@layer-acht.org>
2016-02-11 14:39:41 +0100

    reproducible fdroid: clone upstream git repo again…

--- bin/reproducible_setup_fdroid_build_environment.sh
+++ bin/reproducible_setup_fdroid_build_environment.sh
@@ -15,11 +15,7 @@ common_init "$@"
 
 # define and clean work space (differently than jenkins would normally do as we run via ssh on a different node…)
 WORKSPACE=$BASE/fdroid
-# TODO:
-#
-# add locking here to only run this if no build job is running…
-#
-# not yet needed, as we don't have any build jobs yet
+# FIXME: add locking here to only run this if no build job is running… not yet needed, as we don't have any build jobs yet
 rm $WORKSPACE -rf
 mkdir -p $WORKSPACE
 
@@ -31,26 +27,15 @@ cleanup_all() {
 }
 trap cleanup_all INT TERM EXIT
 
-
-# make sure we have the vagrant box image cached
-test -e ~/.cache/fdroidserver || mkdir -p ~/.cache/fdroidserver
-cd ~/.cache/fdroidserver
-wget --continue https://f-droid.org/jessie32.box || true
-echo "ff6b0c0bebcb742783becbc51a9dfff5a2a0a839bfcbfd0288dcd3113f33e533  jessie32.box" > jessie32.box.sha256
-sha256sum -c jessie32.box.sha256
-
-# wipe the whole vagrant setup and start from scratch
-export VAGRANT_HOME=$WORKSPACE/vagrant.d
-rm -rf $VAGRANT_HOME
-
+# the way we handle jenkins slaves doesn't copy the workspace to the slaves
+# so we need to "manually" clone the git repo here…
 cd $WORKSPACE
+git clone https://gitlab.com/fdroid/fdroidserver.git
 cd fdroidserver
-echo "boot_timeout = 1200" > makebuildserver.config.py
-./makebuildserver 
 
-# we are done here, shutdown
-cd buildserver
-vagrant halt
+# this script is maintained upstream and is also run on Guardian
+# Project's jenkins box
+./jenkins-build-makebuildserver
 
 # remove trap
 trap - INT TERM EXIT

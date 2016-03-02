a51a2a9cdfcebedc9e0c3703080730c73f05e0db
Holger Levsen <holger@layer-acht.org>
2016-03-02 13:28:07 +0100

    torbrowser tests: tab tab tab tab

--- bin/test_torbrowser-launcher.sh
+++ bin/test_torbrowser-launcher.sh
@@ -274,8 +274,8 @@ download_and_launch() {
 		xvkbd -text "\t\t" > /dev/null 2>&1
 		sleep 1
 	elif dpkg --compare-versions $TBL_VERSION gt 0.2.3-1~ ; then
-		echo "$(date -u) - torbrowser-launcher version >0.2.3-1~ detected ($TBL_VERSION), pressing <tab> three times more."
-		xvkbd -text "\t\t\t" > /dev/null 2>&1
+		echo "$(date -u) - torbrowser-launcher version >0.2.3-1~ detected ($TBL_VERSION), pressing <tab> four times more."
+		xvkbd -text "\t\t\t\t" > /dev/null 2>&1
 		sleep 1
 	fi
 	update_screenshot

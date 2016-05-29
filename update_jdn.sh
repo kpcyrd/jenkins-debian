7065b96da04706ff93d2a2cd60ca6d7f875b4234
Philip Hands <phil@hands.com>
2016-05-29 18:40:49 +0200

    lvc: ensure that cucmber et al are installed on pb-build10

--- update_jdn.sh
+++ update_jdn.sh
@@ -241,6 +241,34 @@ if [ -f /etc/debian_version ] ; then
 			   ;;
 			*) ;;
 		esac
+		# cucumber dependencies
+		case $HOSTNAME in
+			profitbricks-build10-amd64|jenkins-test-vm) DEBS="$DEBS
+				cucumber
+				tesseract-ocr
+				i18nspector
+				libav-tools
+				libsikuli-script-java
+				libvirt-dev
+				ovmf
+				python-jabberbot
+				python-potr
+				ruby-guestfs
+				ruby-libvirt
+				ruby-net-irc
+				ruby-packetfu
+				ruby-rb-inotify
+				ruby-rjb
+				ruby-test-unit
+				tcpdump
+				unclutter
+				virt-viewer
+				x264
+				xvfb
+				x11vnc"
+			   ;;
+			*) ;;
+		esac
 		if [ "$HOSTNAME" = "jenkins" ] || [ "$HOSTNAME" = "jenkins-test-vm" ] ; then
 			MASTERDEBS=" 
 				apache2 
@@ -254,7 +282,6 @@ if [ -f /etc/debian_version ] ; then
 				cmake 
 				cron-apt 
 				csvtool 
-				cucumber
 				dnsmasq-base 
 				dose-extra 
 				dstat 
@@ -265,26 +292,19 @@ if [ -f /etc/debian_version ] ; then
 				git-notifier 
 				gocr 
 				graphviz 
-				i18nspector
 				iasl 
 				imagemagick 
 				ip2host
 				jekyll
 				kgb-client
 				libapache2-mod-macro 
-				libav-tools
-				libav-tools 
 				libcap2-bin 
 				libfile-touch-perl 
 				libguestfs-tools 
 				libjson-rpc-perl 
-				libsikuli-script-java
-				libsikuli-script-java 
 				libsoap-lite-perl 
 				libvirt0 
 				libvirt-bin 
-				libvirt-dev
-				libvirt-dev 
 				libvpx1 
 				libxslt1-dev 
 				linux-image-amd64
@@ -297,7 +317,6 @@ if [ -f /etc/debian_version ] ; then
 				openbios-ppc 
 				openbios-sparc 
 				openjdk-7-jre 
-				ovmf
 				pandoc
 				postgresql-client-9.4 
 				poxml 
@@ -307,10 +326,8 @@ if [ -f /etc/debian_version ] ; then
 				python-arpy 
 				python-hachoir-metadata 
 				python-imaging 
-				python-jabberbot
 				python-lzma 
 				python-pip 
-				python-potr
 				python-rpy2 
 				python-setuptools 
 				python-twisted 
@@ -320,39 +337,19 @@ if [ -f /etc/debian_version ] ; then
 				qemu-system-x86 
 				qemu-user-static 
 				radvd 
-				ruby-guestfs
 				ruby-json 
-				ruby-libvirt
-				ruby-libvirt 
-				ruby-net-irc
-				ruby-packetfu
-				ruby-packetfu 
-				ruby-rb-inotify
-				ruby-rjb
-				ruby-rjb 
 				ruby-rspec 
-				ruby-test-unit
 				seabios 
 				shorewall 
 				shorewall6 
 				sqlite3 
 				syslinux 
-				tcpdump
-				tesseract-ocr
 				tor
-				unclutter
-				unclutter 
-				virt-viewer
-				virt-viewer 
 				vncsnapshot 
 				vnstat
 				whohas
 				x11-apps 
-				x11vnc
-				x11vnc
-				x264
 				xtightvncviewer
-				xvfb
 				xvkbd
 				zutils"
 		else

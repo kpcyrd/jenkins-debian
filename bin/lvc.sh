c606484102b4ea7b0dcee18cceaa2aaa93e1143f
Philip Hands <phil@hands.com>
2016-06-05 10:42:37 +0200

    lvc: only check snapshots against files that exist on builder

--- bin/lvc.sh
+++ bin/lvc.sh
@@ -68,7 +68,12 @@ mkdir -p $RESULTS
 
 mkdir -p $WORKSPACE/DebianToasterStorage
 
-discard_snapshots $LIBVIRT_DOMAIN_NAME /srv/jenkins/cucumber /srv/jenkins/bin/lvc.sh /srv/jenkins/job-cfg/lvc.yaml $NETBOOT
+for dep in /srv/jenkins/cucumber /srv/jenkins/bin/lvc.sh /srv/jenkins/job-cfg/lvc.yaml $NETBOOT ; do
+  if [ -e "$dep" ] ; then
+    LV_SNAP_DEPENDS="$LV_SNAP_DEPENDS $dep"
+  fi
+done
+discard_snapshots $LIBVIRT_DOMAIN_NAME $LV_SNAP_DEPENDS
 
 trap cleanup_all INT TERM EXIT
 

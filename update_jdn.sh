c955c27e4a309e319ef1be667407cbc4a55f71a8
Holger Levsen <holger@layer-acht.org>
2016-06-10 22:58:27 +0200

    reproducible fedora: make first deployment work

--- update_jdn.sh
+++ update_jdn.sh
@@ -367,7 +367,8 @@ if [ -f /etc/debian_version ] ; then
 		#		botch
 		# we need mock from bpo to build current fedora
 		if [ "$HOSTNAME" = "profitbricks-build3-amd64" ] || [ "$HOSTNAME" = "profitbricks-build4-amd64" ] || [ "$HOSTNAME" = "jenkins" ] ; then
-			sudo apt-get install -t jessie-backports mock
+			sudo apt-get install -t jessie-backports mock \
+				|| echo "this should only fail on the first install"
 		fi
 		# for varying kernels
 		# we use bpo kernels on pb-build5+6 (and i386 kernel on pb-build2-i386)

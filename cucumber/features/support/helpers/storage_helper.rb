27c697a62d3e180f81751b854d1f8d573f5260e8
Philip Hands <phil@hands.com>
2016-06-16 20:42:25 +0200

    lvc: name the storage after the VM, to avoid conflicts
    perhaps it would be better to prefix volume names in a common pool
    so we could try that at some point
    
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- cucumber/features/support/helpers/storage_helper.rb
+++ cucumber/features/support/helpers/storage_helper.rb
@@ -17,7 +17,8 @@ class VMStorage
     @virt = virt
     @xml_path = xml_path
     pool_xml = REXML::Document.new(File.read("#{@xml_path}/storage_pool.xml"))
-    pool_name = pool_xml.elements['pool/name'].text
+    pool_name = LIBVIRT_DOMAIN_NAME
+    pool_xml.elements['pool/name'].text = pool_name
     @pool_path = "#{$config["TMPDIR"]}/#{pool_name}"
     begin
       @pool = @virt.lookup_storage_pool_by_name(pool_name)

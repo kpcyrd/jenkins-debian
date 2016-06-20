f17066d32c0f0cdd49555881564bbc17049e3c43
Holger Levsen <holger@layer-acht.org>
2016-06-20 21:09:25 +0200

    d-i-build: allow mkdir in /srv/udebs via ssh

--- bin/jenkins_node_wrapper.sh
+++ bin/jenkins_node_wrapper.sh
@@ -89,6 +89,8 @@ elif [[ "$*" =~ rsync\ --server\ --sender\ .*\ .\ /var/lib/jenkins/userContent/r
 	exec rsync --server --sender "$4" . "$6" ; croak "Exec failed";
 elif [[ "$*" =~ rsync\ --server\ --sender\ .*\ .\ /srv/udebs/.*/ ]] ; then
 	exec rsync --server --sender "$4" . "$6" ; croak "Exec failed";
+elif [[ "$*" =~ mkdir -p /srv/udebs/.* ]] ; then
+	exec mkdir -p "$3"  ; croak "Exec failed";
 elif [[ "$*" =~ rm\ -r\ /srv/reproducible-results/tmp.* ]] ; then
 	exec rm -r "$3" ; croak "Exec failed";
 elif [[ "$*" =~ rm\ -r\ /srv/reproducible-results/rbuild.* ]] ; then

8fedb4c47aebe34804d64233ca0abd81975117f9
Philip Hands <phil@hands.com>
2016-06-27 15:03:48 +0200

    lvc: try another way of getting two parameters for the trigger
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/lvc_retreive_artifacts.sh
+++ bin/lvc_retreive_artifacts.sh
@@ -9,6 +9,6 @@ common_init "$@"
 
 # main
 echo "$(date -u) - Starting to rsync results."
-rsync -r -v -e "ssh -o 'Batchmode = yes'" $TRIG_HOST:$TRIG_RESULTS/ $TRIG_RESULTS/
-chmod 775 $RESULTS
+rsync -r -v -e "ssh -o 'Batchmode = yes'" $TRIG_NODE:$TRIG_RESULTS/ /$TRIG_RESULTS/
+chmod 775 $TRIG_RESULTS
 echo "$(date -u) - the end."

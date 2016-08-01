4359bc14b77522046547f877725650689cf7b5f2
Holger Levsen <holger@layer-acht.org>
2016-07-31 23:19:16 -0400

    reproducible Debian: stop all the builds

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -37,8 +37,8 @@ _log () {
 
 exit_early_if_debian_is_broken() {
 	# debian is fine, thanks
-	if false && [ "$ARCH" = "armhf" ] ; then
-		echo "Temporarily stopping the builds on armhf due to #827724… sleeping 12h now…"
+	if true || [ "$ARCH" = "armhf" ] ; then
+		echo "Temporarily stopping the builds due to the new dpkg upload… sleeping 12h now…"
 		for i in $(seq 1 12) ; do
 			sleep 1h
 			echo "one hour passed…."

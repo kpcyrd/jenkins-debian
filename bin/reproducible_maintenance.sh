f9076b64f18cee60495cc8cb47cf0c2f9f676c35
Philip Hands <phil@hands.com>
2016-08-03 23:49:42 +0200

    cope with machines with multiple network interfaces
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_maintenance.sh
+++ bin/reproducible_maintenance.sh
@@ -45,7 +45,7 @@ fi
 echo "$(date -u) - testing whether the proxy works..."
 curl http://www.debian.org > /dev/null
 echo "$(date -u) - testing whether the network interfaces MTU is 1500..."
-if [ "$(ip link | sed -n '/LOOPBACK/!s/.* mtu \([0-9]*\) .*/\1/p')" != "1500" ] ; then
+if [ "$(ip link | sed -n '/LOOPBACK/!s/.* mtu \([0-9]*\) .*/\1/p' | sort -u)" != "1500" ] ; then
 	ip link
 	echo "$(date -u) - network interfaces MTU != 1500 - this is wrong."
 	exit 1

69bb44f13854b87235680f4f56040da42cf568b4
Holger Levsen <holger@layer-acht.org>
2016-09-29 01:03:06 +0200

    reproducible debian: remove old files which should not be there

--- bin/reproducible_html_breakages.py
+++ bin/reproducible_html_breakages.py
@@ -161,7 +161,8 @@ def alien_log(directory=None):
                 try:
                     if os.path.getmtime('/'.join([root, file]))<time.time()-86400:
                         bad_files.append('/'.join([root, file]))
-                        log.warning('/'.join([root, file]) + ' should not be there and older than a day.')
+                        os.remove('/'.join([root, file]))
+                        log.warning('/'.join([root, file]) + ' should not be there and and was older than a day so it was removed.')
                     else:
                         log.info('ignoring ' + '/'.join([root, file]) + ' which should not be there, but is also less than 24h old and will probably soon be gone.')
                 except FileNotFoundError:

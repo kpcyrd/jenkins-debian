bc0570515f6a3dd45f67b4dd660f210bb03ce9a1
Philip Hands <phil@hands.com>
2016-11-01 09:37:13 +0100

    jenkins-test-vm: enable jobs for debian-installer-utils
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- job-cfg/d-i.yaml.py
+++ job-cfg/d-i.yaml.py
@@ -188,7 +188,7 @@ jobs = [
 ]
 
 if "jenkins-test-vm" == os.uname()[1]:
-    pkgs = [ 'debian-installer', 'preseed', 'debootstrap' ]
+    pkgs = [ 'debian-installer', 'preseed', 'debootstrap', 'debian-installer-utils' ]
     jobs = [ '{name}_build-group', '{name}_pu-build-group']
 
 # add a special job for triggering from the pu/ branches

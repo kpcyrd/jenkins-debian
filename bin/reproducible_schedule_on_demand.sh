0229ad55e383ec0a7ed6f08444000a4324ecb676
Daniel Shahaf <danielsh@apache.org>
2016-11-19 09:22:27 +0000

    reproducible Debian: don't split arguments having embedded spaces

--- bin/reproducible_schedule_on_demand.sh
+++ bin/reproducible_schedule_on_demand.sh
@@ -13,12 +13,10 @@ else
 	exit 1
 fi
 
+export LC_USER="$REQUESTER"
+export LOCAL_CALL="true"
 if [ -z "$1" ] ; then
-	PARAMS="--help"
+        /srv/jenkins/bin/reproducible_remote_scheduler.py --help
 else
-	PARAMS="$@"
+        /srv/jenkins/bin/reproducible_remote_scheduler.py "$@"
 fi
-
-LC_USER="$REQUESTER" \
-LOCAL_CALL="true" \
-/srv/jenkins/bin/reproducible_remote_scheduler.py $PARAMS

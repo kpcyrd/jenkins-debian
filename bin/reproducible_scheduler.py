91c1776c1a857bdde657bdc93df0a15590fd57bb
Holger Levsen <holger@layer-acht.org>
2016-11-21 15:34:23 +0100

    reproducible Debian: adjust experimental scheduling frequency to old results in unstable
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_scheduler.py
+++ bin/reproducible_scheduler.py
@@ -65,7 +65,7 @@ LIMITS_404 defines how many packages with status 404 are rescheduled at max.
 
 """
 # only old packages older than this will be rescheduled
-MINIMUM_AGE = {'amd64': 10, 'i386': 10, 'armhf': 15}
+MINIMUM_AGE = {'amd64': 14, 'i386': 20, 'armhf': 20}
 # maximum queue size, see explainations above
 MAXIMA = {'amd64': 750, 'i386': 750, 'armhf': 750}
 # limits, see explainations above

03a6b4fd7c814ec1de5d02c239c0d473be25db73
Philip Hands <phil@hands.com>
2016-11-27 17:21:04 +0100

    lvc: recognise the DebianEdu VT login

--- cucumber/features/step_definitions/common_steps.rb
+++ cucumber/features/step_definitions/common_steps.rb
@@ -653,7 +653,7 @@ Given /^I should see a ([a-zA-Z]*) Login prompt$/ do |style|
       'KDE'  => [ 'DebianLoginPromptKDE.png',  'DebianLoginPromptKDE_d9.png' ],
       'LXDE' => [ 'DebianLoginPromptLXDE.png', 'DebianLoginPromptLXDE_d9.png' ],
       'Gnome' => [ 'DebianLoginPromptGnome.png' ],
-      'VT'   => [ 'DebianLoginPromptVT.png', 'DebianLoginPromptVT_d9.png' ],
+      'VT'   => [ 'DebianLoginPromptVT.png', 'DebianLoginPromptVT_d9.png', 'DebianLoginPromptVT_Edu.png' ],
     }
   end
 

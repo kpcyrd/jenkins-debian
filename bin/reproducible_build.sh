12d0c9e49cca5a0d4a2118b399d3fa80b3f93814
Holger Levsen <holger@layer-acht.org>
2016-12-03 13:38:32 +0100

    reproducible Debian: better use echo in cleanup function, else rbuild.log gets overwritten

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -100,7 +100,7 @@ save_artifacts() {
 }
 
 cleanup_all() {
-	log_info "Starting cleanup."
+	echo "Starting cleanup."
 	if [ "$SAVE_ARTIFACTS" = "1" ] ; then
 		save_artifacts
 	elif [ ! -z "$NOTIFY" ] && [ "$SAVE_ARTIFACTS" = "0" ] ; then
@@ -114,7 +114,7 @@ cleanup_all() {
 		cd
 		rm -r $TMPDIR || true
 	fi
-	log_info "All cleanup done."
+	echo "All cleanup done."
 }
 
 update_db_and_html() {

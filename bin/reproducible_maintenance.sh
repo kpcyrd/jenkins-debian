ce9d61f53c49f8bcd937fd5add90bce04e1ef84e
Holger Levsen <holger@layer-acht.org>
2016-12-03 21:12:50 +0100

    reproducible Debian: drop check for MTU!=1500, this is appearantly fine…
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_maintenance.sh
+++ bin/reproducible_maintenance.sh
@@ -56,14 +56,6 @@ if [ $? -ne 0 ] ; then
 	irc_message debian-reproducible "Proxy is down for $HOSTNAME, please tell the jenkins admins to fix this. (sudo service squid3 restart)"
 	exit 1
 fi
-# check for correct MTU
-echo "$(date -u) - testing whether the network interfaces MTU is 1500..."
-if [ "$(ip link | sed -n '/LOOPBACK\|NOARP/!s/.* mtu \([0-9]*\) .*/\1/p' | sort -u)" != "1500" ] ; then
-	ip link
-	echo "$(date -u) - network interfaces MTU != 1500 - this is wrong.  => please \`sudo ifconfig eth0 mtu 1500\`"
-	irc_message debian-reproducible "$HOSTNAME has wrong MTU, please tell the jenkins admins to fix this.  (sudo ifconfig eth0 mtu 1500)"
-	exit 1
-fi
 
 echo "$(date -u) - updating the schroots and pbuilder now..."
 # use host architecture (only)

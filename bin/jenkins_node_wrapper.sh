908faa58f14fdd762fc76af9adc7df1da568d706
HW42 <hw42@ipsumj.de>
2016-12-08 18:39:58 +0100

    reproducible Debian: allow fifth argument to reproducible_build.sh in node_wrapper
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/jenkins_node_wrapper.sh
+++ bin/jenkins_node_wrapper.sh
@@ -81,7 +81,7 @@ elif [[ "$*" =~ ^lvc_.* ]] ; then
 elif [ "$*" = "reproducible_nodes_info" ] ; then
 	exec /srv/jenkins/bin/reproducible_info.sh ; croak "Exec failed";
 elif [ "$1" = "/srv/jenkins/bin/reproducible_build.sh" ] && ( [ "$2" = "1" ] || [ "$2" = "2" ] ) ; then
-	exec /srv/jenkins/bin/reproducible_build.sh "$2" "$3" "$4" "$5" ; croak "Exec failed";
+	exec /srv/jenkins/bin/reproducible_build.sh "$2" "$3" "$4" "$5" "$6" ; croak "Exec failed";
 elif [[ "$*" =~ ^rsync\ --server\ --sender\ .*\ \.\ /srv/reproducible-results/.* ]] ; then
 	exec rsync --server --sender "$4" . "$6" ; croak "Exec failed";
 elif [[ "$*" =~ ^rsync\ --server\ --sender\ .*\ \.\ /var/lib/jenkins/userContent/reproducible/.* ]] ; then

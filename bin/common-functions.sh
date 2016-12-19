e1d737fd970e08d7526b99b43dc764de07c64dfc
Holger Levsen <holger@layer-acht.org>
2016-12-19 12:56:41 +0100

    stop querying the bts for #848422 - we know it's fixed
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/common-functions.sh
+++ bin/common-functions.sh
@@ -51,18 +51,18 @@ if [ "${0:0:5}" != "/tmp/" ] ; then
 	fi
 	# abort certain jobs if we know they will fail due to certain bugs…
 	case $JOB_NAME in
-		lintian-tests_sid)
-			abort_if_bug_is_still_open 848422 ;;
-		haskell-package-plan)
-			abort_if_bug_is_still_open 848422 ;;
-		edu-packages_sid*)
-			abort_if_bug_is_still_open 848422 ;;
-		reproducible_*_from_git_master)
-			abort_if_bug_is_still_open 848422 ;;
-		chroot-installation_sid_install_education*)
-			abort_if_bug_is_still_open 848422 ;;
-		chroot-installation_stretch_install_education-*_upgrade_to_sid)
-			abort_if_bug_is_still_open 848422 ;;
+		#lintian-tests_sid)
+		#	abort_if_bug_is_still_open 848422 ;;
+		#haskell-package-plan)
+		#	abort_if_bug_is_still_open 848422 ;;
+		#edu-packages_sid*)
+		#	abort_if_bug_is_still_open 848422 ;;
+		#reproducible_*_from_git_master)
+		#	abort_if_bug_is_still_open 848422 ;;
+		#chroot-installation_sid_install_education*)
+		#	abort_if_bug_is_still_open 848422 ;;
+		#chroot-installation_stretch_install_education-*_upgrade_to_sid)
+		#	abort_if_bug_is_still_open 848422 ;;
 		*) ;;
 	esac
 	# mktemp some place for us...

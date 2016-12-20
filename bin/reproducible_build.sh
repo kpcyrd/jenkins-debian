4e7a6f1e1f43517004ef50487b3b53ab86ef724e
Holger Levsen <holger@layer-acht.org>
2016-12-20 11:37:49 +0100

    reproducible Debian: only unregister a build if SRCPKGID is set
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -310,7 +310,9 @@ handle_reproducible() {
 
 unregister_build() {
 	# unregister this build so it will immeditiatly tried again
-	query_db "UPDATE schedule SET date_build_started = NULL, job = NULL WHERE package_id=$SRCPKGID"
+	if [ -n "$SRCPKGID" ] ; then
+		query_db "UPDATE schedule SET date_build_started = NULL, job = NULL WHERE package_id=$SRCPKGID"
+	fi
 	NOTIFY=""
 }
 

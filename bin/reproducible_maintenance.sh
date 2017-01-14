ebdf02023335802d6b5110d748769a558f9c370d
Holger Levsen <holger@layer-acht.org>
2017-01-14 22:22:25 +0100

    reproducible Debian: disable irc notifications for shm problems
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_maintenance.sh
+++ bin/reproducible_maintenance.sh
@@ -76,14 +76,14 @@ fi
 echo "$(date -u) - testing whether /dev/shm is mounted correctly..."
 mount | egrep -q "^tmpfs on /dev/shm" && test "$(stat -c %a -L /dev/shm)" = 1777
 if [ $? -ne 0 ] ; then
-	irc_message debian-reproducible "Warning: /dev/shm is not mounted correctly on $HOSTNAME, please tell the jenkins admins to fix this."
+	echo "Warning: /dev/shm is not mounted correctly on $HOSTNAME, please tell the jenkins admins to fix this."
 	show_fstab_and_mounts
 fi
 # check for /run/shm being mounted properly
 echo "$(date -u) - testing whether /run/shm is mounted correctly..."
 mount | egrep -q "^tmpfs on /run/shm" && test "$(stat -c %a -L /run/shm)" = 1777
 if [ $? -ne 0 ] ; then
-	irc_message debian-reproducible "Warning: /run/shm is not mounted correctly on $HOSTNAME, please tell the jenkins admins to fix this."
+	echo "Warning: /run/shm is not mounted correctly on $HOSTNAME, please tell the jenkins admins to fix this."
 	show_fstab_and_mounts
 fi
 

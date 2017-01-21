746426557751daf9705d0ac0b0750d13e5d37a41
Holger Levsen <holger@layer-acht.org>
2017-01-21 15:33:32 +0100

    reproducible Debian: refactor and don't fail if ntpd ain't running
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- update_jdn.sh
+++ update_jdn.sh
@@ -26,7 +26,11 @@ explain() {
 	echo "$HOSTNAME: $1"
 }
 
-
+set_correct_date() {
+		# set correct date
+		sudo service ntp stop || true
+		sudo ntpdate -b de.pool.ntp.org
+}
 
 echo "--------------------------------------------"
 explain "$(date) - begin deployment update."
@@ -35,13 +39,11 @@ explain "$(date) - begin deployment update."
 case $HOSTNAME in
 	profitbricks-build4-amd64|profitbricks-build5-amd64|profitbricks-build6-i386|profitbricks-build15-amd64|profitbricks-build16-i386)
 		# set correct date
-		sudo service ntp stop
-		sudo ntpdate -b de.pool.ntp.org
+		set_correct_date
 		;;
 	codethink-sled9*|codethink-sled11*|codethink-sled13*|codethink-sled15*)
 		# set correct date
-		sudo service ntp stop
-		sudo ntpdate -b de.pool.ntp.org
+		set_correct_date
 		;;
 	*)	;;
 esac

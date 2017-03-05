d3928b128c348e8f7bccc822312e909bf3b79438
Holger Levsen <holger@layer-acht.org>
2017-03-05 12:51:13 +0100

    reproducible Debian: adjust scheduling frequency, mostly to adopt to faster armhf nodes
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_scheduler.py
+++ bin/reproducible_scheduler.py
@@ -64,7 +64,7 @@ LIMITS_404 defines how many packages with status 404 are rescheduled at max.
 
 """
 # only old packages older than this will be rescheduled
-MINIMUM_AGE = {'amd64': 28, 'i386': 33, 'arm64': 21, 'armhf':32 }
+MINIMUM_AGE = {'amd64': 28, 'i386': 30, 'arm64': 22, 'armhf':28 }
 # maximum queue size, see explainations above
 MAXIMA = {'amd64': 750, 'i386': 750, 'arm64': 1000, 'armhf': 750}
 # limits, see explainations above

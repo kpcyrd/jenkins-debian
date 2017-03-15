d4663d07098d0bf105b2d5bf73488a4c3a2d1774
Holger Levsen <holger@layer-acht.org>
2017-03-15 10:51:16 +0100

    reproducible Debian: detect another case of ENOSPACE causing ftbfs
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -264,6 +264,7 @@ handle_ftbfs() {
 			'^mkdir .*: No space left on device' \
 			'exceeds available storage space.*\(No space left on device\)$' \
 			'^dpkg-source: error: cannot create directory .* No space left on device$' \
+			'Requested size .* exceeds available storage space .*\(No space left on device\)$' \
 			; do
 			if zgrep -e "$NEEDLE" "$DEBIAN_BASE/logs/$SUITE/$ARCH/${SRCPACKAGE}_${EVERSION}.build${BUILD}.log.gz" ; then
 				handle_enospace $node

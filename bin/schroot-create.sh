d933ac20a638accb01dd063dedb56be26d78a9dd
Holger Levsen <holger@layer-acht.org>
2017-03-16 10:24:52 +0100

    reproducible Debian: try to install+use diffoscope from experimental
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/schroot-create.sh
+++ bin/schroot-create.sh
@@ -181,8 +181,14 @@ bootstrap() {
 		fi
 		robust_chroot_apt install -y --no-install-recommends sudo
 		robust_chroot_apt install -y --no-install-recommends $@
-		# always try to use diffoscope from unstable
-		if [ "$SUITE" = "testing" ] && [ "$1" = "diffoscope" ] ; then
+		# try to use diffoscope from experimental
+		if ([ "$SUITE" = "unstable" ] || [ "$SUITE" = "testing" ] ) && [ "$1" = "diffoscope" ] ; then
+			echo "deb $MIRROR experimental main"        | sudo tee -a $SCHROOT_TARGET/etc/apt/sources.list > /dev/null
+			robust_chroot_apt update
+			# install diffoscope from experimental without re-adding all recommends...
+			sudo chroot $SCHROOT_TARGET apt-get install -y -t experimental --no-install-recommends diffoscope || echo "Warning: diffoscope from experimental is uninstallable at the moment."
+		elif [ "$SUITE" = "testing" ] && [ "$1" = "diffoscope" ] ; then
+			# always try to use diffoscope from unstable on testing
 			echo "deb $MIRROR unstable main"        | sudo tee -a $SCHROOT_TARGET/etc/apt/sources.list > /dev/null
 			robust_chroot_apt update
 			# install diffoscope from unstable without re-adding all recommends...

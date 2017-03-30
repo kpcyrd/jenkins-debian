00c3c9a068db1c469424e60e1440789d5304f0b9
Holger Levsen <holger@layer-acht.org>
2017-03-30 17:09:30 +0200

    fixup 34c1585: reproducible Debian: remove unattended upgrades if its installed
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- update_jdn.sh
+++ update_jdn.sh
@@ -498,7 +498,7 @@ if [ -f /etc/debian_version ] ; then
 		fi
 		# remove unattended-upgrades if it's installed
 		if [ -n "$(dpkg -l unattended-upgrades 2>/dev/null|egrep 'ii.*unattended-upgrades')" ] ; then
-			 apt-get -y purge unattended-upgrades
+			 sudo apt-get -y purge unattended-upgrades
 		fi
 		# we need mock from bpo to build current fedora
 		if [ "$HOSTNAME" = "profitbricks-build3-amd64" ] || [ "$HOSTNAME" = "profitbricks-build4-amd64" ] || [ "$HOSTNAME" = "jenkins" ] ; then

1f627b1f21be0286c879a90b75186a21493cef81
Holger Levsen <holger@layer-acht.org>
2017-03-30 17:40:20 +0200

    fix regex to also match display/redirect
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/email2irc.sh
+++ bin/email2irc.sh
@@ -128,7 +128,7 @@ if [ "$VALID_MAIL" = "true" ] ; then
 		MESSAGE="$(echo $MESSAGE | sed -s 's#\?page=changes$##') "
 		MESSAGE="$(echo $MESSAGE | sed -s 's#/console$##') "
 		MESSAGE="$(echo $MESSAGE | sed -s 's#/changes$##') "
-		MESSAGE="$(echo $MESSAGE | sed -s 's#display/redirect.*\>$##') "
+		MESSAGE="$(echo $MESSAGE | sed -s 's#display/redirec.*\>$##') "
 		# log message
 		echo "Notified #$CHANNEL with $MESSAGE" >> $LOGFILE
 		# notify kgb

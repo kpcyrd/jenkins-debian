d0db59ed0007fa7612d66c628bb59d275432c825
Holger Levsen <holger@layer-acht.org>
2017-04-10 21:21:23 +0200

    whitespace change to force job config regeneration as needed by latest jenkins plugin security updates
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- job-cfg/d-i.yaml.py
+++ job-cfg/d-i.yaml.py
@@ -483,7 +483,7 @@ data.append(
     {'project': {
         'name': 'd-i',
         'do_not_edit': (
-            '<br><br>Job configuration source is '
+            '<br><br>Job  configuration source is '
             '<a href="https://anonscm.debian.org/git/qa/'
             'jenkins.debian.net.git/tree/job-cfg/d-i.yaml.py">'
             'd-i.yaml.py</a>.'),

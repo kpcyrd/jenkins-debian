8f255e38f5490b639553eca734c09901663ef23a
Holger Levsen <holger@layer-acht.org>
2017-04-15 16:33:15 +0200

    reproducible Debian: fix assumption and requirements
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_build_service.sh
+++ bin/reproducible_build_service.sh
@@ -19,7 +19,7 @@ set -x
 /bin/sleep $(echo "scale=1 ; $(shuf -i 1-230 -n 1)/10" | bc )
 
 BUILD_BASE=/var/lib/jenkins/userContent/reproducible/debian/build_service/$1
-OLD_ID=$((ls -1rt $BUILD_BASE||echo 0)|tail -1)
+OLD_ID=$(ls -1rt $BUILD_BASE|grep -v latest|tail -1)
 let BUILD_ID=OLD_ID+1
 mkdir -p $BUILD_BASE/$BUILD_ID
 ln -sf $BUILD_BASE/$BUILD_ID $BUILD_BASE/latest

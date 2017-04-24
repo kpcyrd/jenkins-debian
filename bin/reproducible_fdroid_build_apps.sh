67719b1cdb0beb7404e300361281fa9f3a19a6f2
Hans-Christoph Steiner <hans@eds.org>
2017-04-24 12:26:50 +0200

    reproducible fdroid: move script to fdroid git repo for fast test cycles
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_fdroid_build_apps.sh
+++ bin/reproducible_fdroid_build_apps.sh
@@ -27,52 +27,7 @@ cleanup_all() {
 }
 trap cleanup_all INT TERM EXIT
 
-# report info about virtualization
-(dmesg | grep -i -e hypervisor -e qemu -e kvm) || true
-(lspci | grep -i -e virtio -e virtualbox -e qemu -e kvm) || true
-lsmod
-if systemd-detect-virt -q ; then
-        echo "Virtualization is used:" `systemd-detect-virt`
-else
-        echo "No virtualization is used."
-fi
-sudo /bin/chmod -R a+rX /var/lib/libvirt/images
-ls -ld /var/lib/libvirt/images
-ls -l /var/lib/libvirt/images || echo no access
-ls -lR ~/.vagrant.d/ || echo no access
-virsh --connect qemu:///system list --all || echo cannot virsh list
-cat /etc/issue
-
-# point to the Vagrant/VirtualBox configs created by reproducible_setup_fdroid_build_environment.sh
-# these variables are actually set in fdroidserver/jenkins-build-makebuildserver
-SETUP_WORKSPACE=$BASE/fdroid
-export XDG_CONFIG_HOME=$SETUP_WORKSPACE
-export VBOX_USER_HOME=$SETUP_WORKSPACE/VirtualBox
-export VAGRANT_HOME=$SETUP_WORKSPACE/vagrant.d
-
-# the way we handle jenkins slaves doesn't copy the workspace to the slaves
-# so we need to "manually" clone the git repo here…
-cd $WORKSPACE
-#git clone https://gitlab.com/fdroid/fdroidserver.git
-git clone https://gitlab.com/uniqx/fdroidserver.git
-cd fdroidserver
-git checkout jenkins.debian.net # normally master too
-
-# set up Android SDK to use the Debian packages in stretch
-export ANDROID_HOME=/usr/lib/android-sdk
-
-# ignore username/password prompt for non-existant repos
-git config --global url."https://fakeusername:fakepassword@github.com".insteadOf https://github.com
-git config --global url."https://fakeusername:fakepassword@gitlab.com".insteadOf https://gitlab.com
-git config --global url."https://fakeusername:fakepassword@bitbucket.org".insteadOf https://bitbucket.org
-
-# now build the whole archive
-cd $WORKSPACE
-git clone https://gitlab.com/fdroid/fdroiddata.git
-cd fdroiddata
-echo "build_server_always = True" > config.py
-$WORKSPACE/fdroidserver/fdroid build --verbose --latest --all
-
+./jenkins-build
 
 # remove trap
 trap - INT TERM EXIT

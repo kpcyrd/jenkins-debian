e3cffd87fecc161196a384e747e0f95496ce5c4f
Holger Levsen <holger@layer-acht.org>
2017-04-26 14:23:42 +0200

    reproducible Debian: enable service workers for armhf and disable jenkins builder jobs
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_build_service.sh
+++ bin/reproducible_build_service.sh
@@ -65,6 +65,80 @@ choose_node() {
                 arm64_30)       NODE1=codethink-sled15-arm64    NODE2=codethink-sled12-arm64 ;;
                 arm64_31)       NODE1=codethink-sled14-arm64    NODE2=codethink-sled11-arm64 ;;
                 arm64_32)       NODE1=codethink-sled13-arm64    NODE2=codethink-sled14-arm64 ;;
+		# to choose new armhf jobs:
+	        #       for i in cb3a hb0 rpi2b rpi2c wbd0 bpi0 bbx15 cbxi4pro0 ff2a ff2b jtk1a odxu4 odxu4b odxu4c odu3a opi2a opi2b opi2c p64b p64c wbq0 cbxi4a cbxi4b ff4a ; do echo "$i: " ; grep NODE1 bin/reproducible_build_service.sh|grep armhf|grep $i-armhf ; done
+	        #       8 jobs for quad-cores with 4 gb ram
+	        #       6 jobs for octo-cores with 2 gb ram
+	        #       6 jobs for quad-cores with 2 gb ram
+	        #       3 jobs for dual-cores with 1 gb ram
+	        #       3 jobs for quad-cores with 1 gb ram
+                armhf_1)        NODE1=bbx15-armhf-rb          NODE2=odxu4-armhf-rb ;;
+                armhf_2)        NODE1=wbq0-armhf-rb           NODE2=p64c-armhf-rb ;;
+                armhf_3)        NODE1=hb0-armhf-rb            NODE2=p64b-armhf-rb ;;
+                armhf_4)        NODE1=ff4a-armhf-rb           NODE2=wbq0-armhf-rb ;;
+                armhf_5)        NODE1=cbxi4pro0-armhf-rb      NODE2=bpi0-armhf-rb ;;
+                armhf_6)        NODE1=ff4a-armhf-rb           NODE2=cbxi4pro0-armhf-rb ;;
+                armhf_7)        NODE1=wbq0-armhf-rb           NODE2=odxu4-armhf-rb ;;
+                armhf_8)        NODE1=hb0-armhf-rb            NODE2=wbq0-armhf-rb ;;
+                armhf_9)        NODE1=ff4a-armhf-rb           NODE2=bpi0-armhf-rb ;;
+                armhf_10)       NODE1=odxu4-armhf-rb          NODE2=rpi2b-armhf-rb ;;
+                armhf_11)       NODE1=odxu4-armhf-rb          NODE2=wbd0-armhf-rb ;;
+                armhf_12)       NODE1=wbd0-armhf-rb           NODE2=cbxi4pro0-armhf-rb ;;
+                armhf_13)       NODE1=cbxi4pro0-armhf-rb      NODE2=rpi2b-armhf-rb ;;
+                armhf_14)       NODE1=cbxi4a-armhf-rb         NODE2=odxu4b-armhf-rb ;;
+                armhf_15)       NODE1=rpi2b-armhf-rb          NODE2=odxu4c-armhf-rb ;;
+                armhf_16)       NODE1=odxu4b-armhf-rb         NODE2=wbd0-armhf-rb ;;
+                armhf_17)       NODE1=odxu4c-armhf-rb         NODE2=hb0-armhf-rb ;;
+                armhf_18)       NODE1=odxu4b-armhf-rb         NODE2=odu3a-armhf-rb ;;
+                armhf_19)       NODE1=odxu4c-armhf-rb         NODE2=opi2c-armhf-rb ;;
+                armhf_20)       NODE1=opi2b-armhf-rb          NODE2=odxu4b-armhf-rb ;;
+                armhf_21)       NODE1=ff2a-armhf-rb           NODE2=odxu4c-armhf-rb ;;
+                armhf_22)       NODE1=ff2a-armhf-rb           NODE2=rpi2c-armhf-rb ;;
+                armhf_23)       NODE1=rpi2c-armhf-rb          NODE2=odxu4b-armhf-rb ;;
+                armhf_24)       NODE1=rpi2c-armhf-rb          NODE2=odxu4c-armhf-rb ;;
+                armhf_25)       NODE1=odxu4b-armhf-rb         NODE2=ff2b-armhf-rb ;;
+                armhf_26)       NODE1=jtk1a-armhf-rb          NODE2=ff2a-armhf-rb ;;
+                armhf_27)       NODE1=odxu4c-armhf-rb         NODE2=cbxi4a-armhf-rb ;;
+                armhf_28)       NODE1=jtk1a-armhf-rb          NODE2=ff2b-armhf-rb ;;
+                armhf_29)       NODE1=ff2b-armhf-rb           NODE2=jtk1a-armhf-rb ;;
+                armhf_30)       NODE1=ff2b-armhf-rb           NODE2=cbxi4b-armhf-rb ;;
+                armhf_31)       NODE1=ff2b-armhf-rb           NODE2=opi2b-armhf-rb ;;
+                armhf_32)       NODE1=jtk1a-armhf-rb          NODE2=cbxi4b-armhf-rb ;;
+                armhf_33)       NODE1=ff2a-armhf-rb           NODE2=opi2b-armhf-rb ;;
+                armhf_34)       NODE1=cbxi4a-armhf-rb         NODE2=opi2b-armhf-rb ;;
+                armhf_35)       NODE1=opi2a-armhf-rb          NODE2=ff2b-armhf-rb ;;
+                armhf_36)       NODE1=opi2a-armhf-rb          NODE2=cbxi4a-armhf-rb ;;
+                armhf_37)       NODE1=opi2a-armhf-rb          NODE2=wbq0-armhf-rb ;;
+                armhf_38)       NODE1=cbxi4b-armhf-rb         NODE2=jtk1a-armhf-rb ;;
+                armhf_39)       NODE1=cbxi4b-armhf-rb         NODE2=cbxi4a-armhf-rb ;;
+                armhf_40)       NODE1=opi2b-armhf-rb          NODE2=cbxi4b-armhf-rb ;;
+                armhf_41)       NODE1=opi2b-armhf-rb          NODE2=cbxi4b-armhf-rb ;;
+                armhf_42)       NODE1=cbxi4b-armhf-rb         NODE2=cbxi4a-armhf-rb ;;
+                armhf_43)       NODE1=cbxi4a-armhf-rb         NODE2=opi2c-armhf-rb ;;
+                armhf_44)       NODE1=bbx15-armhf-rb          NODE2=ff4a-armhf-rb ;;
+                armhf_45)       NODE1=ff4a-armhf-rb           NODE2=p64b-armhf-rb ;;
+                armhf_46)       NODE1=wbq0-armhf-rb           NODE2=bbx15-armhf-rb ;;
+                armhf_47)       NODE1=cbxi4pro0-armhf-rb      NODE2=bbx15-armhf-rb ;;
+                armhf_48)       NODE1=bbx15-armhf-rb          NODE2=p64c-armhf-rb ;;
+                armhf_49)       NODE1=bpi0-armhf-rb           NODE2=ff4a-armhf-rb ;;
+                armhf_50)       NODE1=odxu4-armhf-rb          NODE2=odu3a-armhf-rb ;;
+                armhf_51)       NODE1=odu3a-armhf-rb          NODE2=cb3a-armhf-rb ;;
+                armhf_52)       NODE1=opi2c-armhf-rb          NODE2=cb3a-armhf-rb ;;
+                armhf_53)       NODE1=cb3a-armhf-rb           NODE2=ff4a-armhf-rb ;;
+                armhf_54)       NODE1=odu3a-armhf-rb          NODE2=opi2c-armhf-rb ;;
+                armhf_55)       NODE1=opi2c-armhf-rb          NODE2=odu3a-armhf-rb ;;
+                armhf_56)       NODE1=odu3a-armhf-rb          NODE2=ff2a-armhf-rb ;;
+                armhf_57)       NODE1=opi2c-armhf-rb          NODE2=ff2a-armhf-rb ;;
+                armhf_58)       NODE1=cbxi4a-armhf-rb         NODE2=p64b-armhf-rb ;;
+                armhf_59)       NODE1=jtk1a-armhf-rb          NODE2=p64c-armhf-rb ;;
+                armhf_60)       NODE1=cbxi4b-armhf-rb         NODE2=opi2a-armhf-rb ;;
+                armhf_61)       NODE1=p64c-armhf-rb           NODE2=opi2a-armhf-rb ;;
+                armhf_62)       NODE1=p64b-armhf-rb           NODE2=opi2a-armhf-rb ;;
+                armhf_63)       NODE1=p64b-armhf-rb           NODE2=ff4a-armhf-rb ;;
+                armhf_64)       NODE1=p64c-armhf-rb           NODE2=bbx15-armhf-rb ;;
+                armhf_65)       NODE1=p64b-armhf-rb           NODE2=cbxi4pro0-armhf-rb ;;
+                armhf_66)       NODE1=p64c-armhf-rb           NODE2=odxu4-armhf-rb ;;
+
 		*)		echo "Sleeping 60min"
 				sleep 60m
 				exit 0
@@ -78,6 +152,7 @@ for ARCH in i386 arm64 ; do
 	case $ARCH in
 		i386)	MAX=24 ;;
 		arm64)	MAX=32 ;;
+		armhf)	MAX=66 ;;
 		*)	;;
 	esac
 	for i in $(seq 1 $MAX) ; do
@@ -97,9 +172,10 @@ done
 while true ; do sleep 1337m ; done
 
 # TODO left:
-# * translate yaml into a script or such to create those service files (done for i386 for now)
+# * amd64!
+# * enabling the service in update_jdn
 # * logs should auto display in browser like with jenkins… (long-polling, meta-refresh, something)
 #   - there's an NPH solution pointed out by Xtaran
 # * maintenance job might want to:
-#   - check for running builds using systemctl show
+#   - check for running builds using systemctl show & ps fax
 

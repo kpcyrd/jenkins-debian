d8658e15935bc4f43c34703054797887f5d3774e
Holger Levsen <holger@layer-acht.org>
2017-05-03 02:27:26 +0200

    reproducible Debian: yet more scheduling
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_scheduler.py
+++ bin/reproducible_scheduler.py
@@ -64,7 +64,7 @@ LIMITS_404 defines how many packages with status 404 are rescheduled at max.
 
 """
 # only old packages older than this will be rescheduled
-MINIMUM_AGE = {'amd64': 18, 'i386': 21, 'arm64': 18, 'armhf':30 }
+MINIMUM_AGE = {'amd64': 15, 'i386': 20, 'arm64': 15, 'armhf':30 }
 # maximum queue size, see explainations above
 MAXIMA = {'amd64': 750, 'i386': 750, 'arm64': 1000, 'armhf': 750}
 # limits, see explainations above

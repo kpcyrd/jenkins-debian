f844c5d0724c68962588b33476200f93ffb9cbf3
Holger Levsen <holger@layer-acht.org>
2017-06-12 23:54:31 +0200

    reproducible Debian: I believe this is needed to fixup the previous commit…
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/jenkins_node_wrapper.sh
+++ bin/jenkins_node_wrapper.sh
@@ -78,7 +78,7 @@ elif [[ "$*" =~ ^lvc_.* ]] ; then
 	export WORKSPACE=/var/lib/jenkins/jobs/$JOB_NAME/workspace
 	COMMAND="/srv/jenkins/bin/lvc.sh $@"
 	exec $COMMAND; croak "Exec failed";
-elif [ "$*" = "reproducible_nodes_info" ] ; then
+elif [ "$*" = "reproducible_html_nodes_info" ] ; then
 	exec /srv/jenkins/bin/reproducible_info.sh ; croak "Exec failed";
 elif [ "$1" = "/srv/jenkins/bin/reproducible_build.sh" ] && ( [ "$2" = "1" ] || [ "$2" = "2" ] ) ; then
 	exec /srv/jenkins/bin/reproducible_build.sh "$2" "$3" "$4" "$5" "$6" ; croak "Exec failed";

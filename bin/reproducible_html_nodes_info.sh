e656e242d98ee2611a4089045ad8d4d7c70d0548
Holger Levsen <holger@layer-acht.org>
2017-06-13 00:02:42 +0200

    reproducible Debian: refactoring is hard, fixup f02bf39d
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_html_nodes_info.sh
+++ bin/reproducible_html_nodes_info.sh
@@ -25,7 +25,7 @@ SUITE=unstable
 echo "$(date -u) - starting to write $PAGE page."
 write_page_header $VIEW "Nodes health overview"
 DISCLAIMER="<p>This page is still under development. Please provide feedback, which other information (be it from munin or elsewhere) should be displayed and how this page should be split further, eg, the graphs could all be on another page and/or we should split this page into four for the four architectures being tested…</p>"
-write_page $DISCLAIMER
+write_page "$DISCLAIMER"
 write_page "<p style=\"clear:both;\">"
 for ARCH in ${ARCHS} ; do
 	write_page "<h3>$ARCH nodes</h3>"
@@ -104,7 +104,7 @@ for TYPE in daily weekly ; do
 	SUITE=unstable
 	echo "$(date -u) - starting to write $PAGE page."
 	write_page_header $VIEW "Nodes $TYPE graphs"
-	write_page $DISCLAIMER
+	write_page "$DISCLAIMER"
 	write_page "<p style=\"clear:both;\">"
 	for ARCH in ${ARCHS} ; do
 		write_page "<h3>$ARCH nodes</h3>"

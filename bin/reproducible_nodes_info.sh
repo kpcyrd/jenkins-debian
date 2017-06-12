f02bf39d07ceadc3f6882d6ccd2e8a8fe8c2b29e
Holger Levsen <holger@layer-acht.org>
2017-06-12 23:46:16 +0200

    reproducible Debian: refactor
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_nodes_info.sh
+++ bin/reproducible_nodes_info.sh
@@ -24,7 +24,8 @@ ARCH=amd64
 SUITE=unstable
 echo "$(date -u) - starting to write $PAGE page."
 write_page_header $VIEW "Nodes health overview"
-write_page "<p>This page is still under development. Please provide feedback, which other information (be it from munin or elsewhere) should be displayed and how this page should be split further, eg, the graphs could all be on another page and/or we should split this page into four for the four architectures being tested…</p>"
+DISCLAIMER="<p>This page is still under development. Please provide feedback, which other information (be it from munin or elsewhere) should be displayed and how this page should be split further, eg, the graphs could all be on another page and/or we should split this page into four for the four architectures being tested…</p>"
+write_page $DISCLAIMER
 # FIXME: Also either $0 and its job needs to be renamed to include 'html' or the code needs to be moved elsewhere
 write_page "<p style=\"clear:both;\">"
 for ARCH in ${ARCHS} ; do
@@ -104,8 +105,7 @@ for TYPE in daily weekly ; do
 	SUITE=unstable
 	echo "$(date -u) - starting to write $PAGE page."
 	write_page_header $VIEW "Nodes $TYPE graphs"
-	write_page "<p>This page is still under development. Please provide feedback, which other information (be it from munin or elsewhere) should be displayed and how this page should be split further, eg, the graphs could all be on another page and/or we should split this page into four for the four architectures being tested…</p>"
-	# FIXME: Also either $0 and its job needs to be renamed to include 'html' or the code needs to be moved elsewhere
+	write_page $DISCLAIMER
 	write_page "<p style=\"clear:both;\">"
 	for ARCH in ${ARCHS} ; do
 		write_page "<h3>$ARCH nodes</h3>"

93bbca8f030177e33384b819ee768e1ce2a891e5
Holger Levsen <holger@layer-acht.org>
2017-08-08 21:08:45 -0400

    disable debug output
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/chroot-run.sh
+++ bin/chroot-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Copyright 2012-2016 Holger Levsen <holger@layer-acht.org>
+# Copyright 2012-2017 Holger Levsen <holger@layer-acht.org>
 # Copyright      2013 Antonio Terceiro <terceiro@debian.org>
 # released under the GPLv=2
 
-DEBUG=true
+DEBUG=false
 . /srv/jenkins/bin/common-functions.sh
 common_init "$@"
 

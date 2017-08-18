9b252816990adaacd3239d3a0681c07d3b04c56c
Holger Levsen <holger@layer-acht.org>
2017-08-18 08:18:32 -0400

    chroot-installs: ignore texlive-htmlxml (but don't file a removal bug until buster is released) when checking for transitional packages
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/chroot-installation.sh
+++ bin/chroot-installation.sh
@@ -289,11 +289,13 @@ if [ "$DISTRO" = "sid" ] ; then
 	# ignore dh-systemd because #871312
 	# ignore libpcap-dev because #872265
 	# ignore "dummy transitional library" because it really is what it says it is…
-	# ignore libidn2-0-dev (but don't file a removal bug until buster is released)
+	# ignore transitional packages introduced during busters lifecycle (so bugs should only be filed once we released buster)
+	# - libidn2-0-dev
+	# - texlive-htmlxml
 	( sudo chroot $CHROOT_TARGET dpkg -l \
 		| grep -v multiarch-support \
 		| egrep -v "(jadetex|dh-systemd|libpcap-dev)" \
-		| egrep -v libidn2-0-dev \
+		| egrep -v "(libidn2-0-dev|texlive-htmlxml)" \
 		| grep -v "dummy transitional library" \
 		| grep -i "Transitional" 2>/dev/null || true) > $TMPFILE
 	if [ -s $TMPFILE ] ; then

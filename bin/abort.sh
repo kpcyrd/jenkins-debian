71fa4bfb29cf9311d3e67d7410806c05acf24043
Holger Levsen <holger@layer-acht.org>
2017-09-19 13:36:56 +0200

    less verbose aborting
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/abort.sh
+++ bin/abort.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export
+# generally interesting: BUILD_* JENKINS_* JOB_* but most is in BUILD_URL, so:
+export | egrep "(BUILD_URL=)"
 TMPFILE=$(mktemp)
+
 curl https://jenkins.debian.net/jnlpJars/jenkins-cli.jar -o $TMPFILE
 java -jar $TMPFILE -s http://localhost:8080/ set-build-result aborted
 rm $TMPFILE

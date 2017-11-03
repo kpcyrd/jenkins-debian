883f70ada2adb159f8f9c96427c491a03a7d41cc
kpcyrd <git@rxv.cc>
2017-11-03 02:32:08 +0100

    reproducible-archlinux: disable signature verification when running in the future
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- bin/reproducible_setup_archlinux_schroot.sh
+++ bin/reproducible_setup_archlinux_schroot.sh
@@ -100,6 +100,10 @@ echo "Server = $ARCHLINUX_MIRROR/\$repo/os/\$arch" | tee -a $SCHROOT_BASE/$TARGE
 # enable multilib
 # (-0777 tells perl to read the whole file before processing it. then it just does a multi-line regex…)
 perl -0777 -i -pe 's/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/[multilib]\nInclude = \/etc\/pacman.d\/mirrorlist/igs' $SCHROOT_BASE/$TARGET/etc/pacman.conf
+if [ "$HOSTNAME" = "profitbricks-build4-amd64" ] ; then
+    # disable signature verification so packages won't fail to install when setting the time to +$x years
+    sed -i 's/^SigLevel\s*=.*/SigLevel = Never/' "$SCHROOT_BASE/$TARGET/etc/pacman.conf"
+fi
 $ROOTCMD bash -l -c 'pacman -Syu --noconfirm'
 $ROOTCMD bash -l -c 'pacman -S --noconfirm base-devel devtools fakechroot asciidoc wget'
 # configure sudo

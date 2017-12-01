ef73e122a8808103a858f065afefccc204197088
Holger Levsen <holger@layer-acht.org>
2017-12-01 12:32:46 +0000

    reproducible Arch Linux: count and announce amount of newly scheduled packages
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_archlinux_scheduler.sh
+++ bin/reproducible_archlinux_scheduler.sh
@@ -13,6 +13,8 @@ common_init "$@"
 set -e
 
 update_archlinux_repositories() {
+	UPDATED=0
+	NEW=0
 	local SESSION="archlinux-scheduler-$RANDOM"
 	schroot --begin-session --session-name=$SESSION -c jenkins-reproducible-archlinux
 	schroot --run-session -c $SESSION --directory /var/tmp -- sudo pacman -Syu --noconfirm
@@ -31,12 +33,20 @@ update_archlinux_repositories() {
 		echo "$(date -u ) - updating list of available packages in repository '$REPO'."
 		grep "^$REPO" "$ARCHLINUX_PKGS"_full_pkgbase_list | \
 			while read repo pkgbase version; do
-				VERSION=$(cat $BASE/archlinux/$REPO/$pkgbase/pkg.version 2>/dev/null || echo 0.rb-unknown-1)
-				if [ "$VERSION" != "0.rb-unknown-1" ] || [ ! -f $BASE/archlinux/$REPO/$pkgbase/pkg.needs_build ] ; then
-					if [ "$(schroot --run-session -c $SESSION --directory /var/tmp -- vercmp $version $VERSION)" = "1" ] ; then
-						echo -n "$(date -u ) - we know about $REPO/$pkgbase $VERSION, but the repo has $version, so rescheduling... "
-						mkdir -p $BASE/archlinux/$REPO/$pkgbase
-						touch $BASE/archlinux/$REPO/$pkgbase/pkg.needs_build
+				if [ ! -d $BASE/archlinux/$REPO/$pkgbase ] ; then
+					let NEW+=1
+					echo -n "$(date -u ) - scheduling new package $REPO/$pkgbase... "
+					mkdir -p $BASE/archlinux/$REPO/$pkgbase
+					touch $BASE/archlinux/$REPO/$pkgbase/pkg.needs_build
+				else
+					VERSION=$(cat $BASE/archlinux/$REPO/$pkgbase/pkg.version 2>/dev/null || echo 0.rb-unknown-1)
+					if [ "$VERSION" != "0.rb-unknown-1" ] || [ ! -f $BASE/archlinux/$REPO/$pkgbase/pkg.needs_build ] ; then
+						if [ "$(schroot --run-session -c $SESSION --directory /var/tmp -- vercmp $version $VERSION)" = "1" ] ; then
+							let UPDATED+=1
+							echo -n "$(date -u ) - we know about $REPO/$pkgbase $VERSION, but the repo has $version, so rescheduling... "
+							mkdir -p $BASE/archlinux/$REPO/$pkgbase
+							touch $BASE/archlinux/$REPO/$pkgbase/pkg.needs_build
+						fi
 					fi
 				fi
 				printf '%s %s\n' "$pkgbase" "$version"
@@ -46,6 +56,9 @@ update_archlinux_repositories() {
 	done
 	rm "$ARCHLINUX_PKGS"_full_pkgbase_list
 	schroot --end-session -c $SESSION
+	if [ $NEW -ne 0 ] || [ $UPDATED -ne 0 ] ; then
+		irc_message archlinux-reproducible "scheduled $NEW entirely new packages and $UPDATED packages with newer versions."
+	fi
 }
 
 echo "$(date -u ) - Updating Arch Linux repositories."

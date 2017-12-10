8ba4ecf7044a52b498490c6fb6ec7f27f738f770
Holger Levsen <holger@layer-acht.org>
2017-12-10 16:57:16 +0000

    reproducible Arch Linux: sleep 3min at job end to prevent immediate restarts…
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_html_archlinux.sh
+++ bin/reproducible_html_archlinux.sh
@@ -378,4 +378,7 @@ write_page "</div></div>"
 write_page_footer 'Arch Linux'
 echo "$(date -u) - enjoy $REPRODUCIBLE_URL/archlinux/$PAGE"
 
+echo "$(date -u) - Sleeping 3min now to prevent immediate restart of this job…"
+sleep 3m
+
 # vim: set sw=0 noet :
